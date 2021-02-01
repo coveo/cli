00c9c0180debefd6168c46e39aed03a7074f84f9
Yassine Lakhdar <ylakhdar@coveo.com>
2021-02-01 10:42:44 -0500

    tests: mock process function
    https://coveord.atlassian.net/browse/CDX-52

--- packages/cli/src/commands/ui/create/react.spec.ts
+++ packages/cli/src/commands/ui/create/react.spec.ts
@@ -1,10 +1,36 @@
-import {expect, test} from '@oclif/test';
+import {test} from '@oclif/test';
+import {EventEmitter} from 'events';
+import * as child_process from 'child_process';
+
+jest.mock('child_process', () => ({
+  spawn: jest.fn().mockImplementation(() => {
+    const successExitCode = 0;
+    const emitter = new EventEmitter();
+    process.nextTick(() => emitter.emit('close', successExitCode)); // To mock a sucess command
+    return emitter as child_process.ChildProcess;
+  }),
+}));
 
 describe('ui:create:react', () => {
+  const reactAppExecutable = 'create-react-app/index.js';
+  const reactAppCoveoTemplate = 'cra-template-coveo';
+
   test
     .command(['ui:create:react'])
     .catch((ctx) => {
-      expect(ctx.message).to.contain('Missing 1 required arg:');
+      expect(ctx.message).toContain('Missing 1 required arg:');
     })
     .it('requires application name argument');
+
+  test
+    .stdout()
+    .command(['ui:create:react', 'myapp'])
+    .it('should start a spawn process with the appropriate arguments', () => {
+      expect(child_process.spawn).toHaveBeenCalledTimes(1);
+      expect(child_process.spawn).toHaveBeenCalledWith(
+        expect.stringContaining(reactAppExecutable),
+        ['myapp', '--template', expect.stringContaining(reactAppCoveoTemplate)],
+        expect.objectContaining({})
+      );
+    });
 });
