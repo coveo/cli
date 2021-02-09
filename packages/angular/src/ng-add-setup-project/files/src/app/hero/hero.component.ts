de9c8b8813ad373038e2ac16aaa1577c5d2ee9c8
Yassine Lakhdar <ylakhdar@coveo.com>
2021-02-09 14:00:27 -0500

    add last template file
    https://coveord.atlassian.net/browse/CDX-72

---
+++ packages/angular/src/ng-add-setup-project/files/src/app/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss'],
+})
+export class HeroComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
