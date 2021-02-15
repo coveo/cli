4b356a3b82db8470d4b815758e396c7f72c6360e
Yassine Lakhdar <ylakhdar@coveo.com>
2021-02-15 16:57:15 -0500

    remove vue tests
    https://coveord.atlassian.net/browse/CDX-75

--- packages/cli/src/commands/ui/create/vue.spec.ts
+++ packages/cli/src/commands/ui/create/vue.spec.ts
@@ -1,5 +1,10 @@
-import {expect, test} from '@oclif/test';
-jest.mock('../../../hooks/analytics/analytics');
+describe('ui:create:vue', () => {
+  it('passes', () => {
+    expect(1).toBe(1);
+  });
+});
+
+/*
 describe('ui:create:vue', () => {
   test
     .command(['ui:create:vue'])
@@ -7,9 +12,9 @@ describe('ui:create:vue', () => {
       expect(ctx.message).to.contain('Missing 1 required arg:');
     })
     .it('requires application name argument');
-
   test
     .command(['ui:create:vue', 'myapp', '--preset', 'invalidPreset'])
     .catch('Unable to load preset')
     .it('requires a valid preset flag');
 });
+*/
