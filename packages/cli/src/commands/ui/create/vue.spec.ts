f6cecdd3e30be2ae66af3a2a8f5c24ca37b6819e
Yassine Lakhdar <ylakhdar@coveo.com>
2021-02-16 14:18:20 -0500

    remove file from change set
    https://coveord.atlassian.net/browse/CDX-75

--- packages/cli/src/commands/ui/create/vue.spec.ts
+++ packages/cli/src/commands/ui/create/vue.spec.ts
@@ -11,6 +11,7 @@ describe('ui:create:vue', () => {
     .catch((ctx) => {
       expect(ctx.message).to.contain('Missing 1 required arg:');
     })
+
     .it('requires application name argument');
   test
     .command(['ui:create:vue', 'myapp', '--preset', 'invalidPreset'])
