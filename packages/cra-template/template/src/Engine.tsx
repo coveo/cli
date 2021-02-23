7aec1dd37f75be44e3e5b71cd9d8dc47dd5ea3a5
Yassine <ylakhdar@coveo.com>
2021-02-23 12:09:52 -0500

    feat(cli): connect React project with client's org (#36)
    https://coveord.atlassian.net/browse/CDX-61

--- packages/cra-template/template/src/Engine.tsx
+++ packages/cra-template/template/src/Engine.tsx
@@ -1,6 +1,15 @@
 import {HeadlessEngine, searchAppReducers} from '@coveo/headless';
 
 export const headlessEngine = new HeadlessEngine({
-  configuration: HeadlessEngine.getSampleConfiguration(),
+  configuration: {
+    platformUrl: process.env.REACT_APP_PLATFORM_URL,
+    organizationId: process.env.REACT_APP_ORGANIZATION_ID!,
+    accessToken: process.env.REACT_APP_API_KEY!,
+    renewAccessToken: async () => {
+      const res = await fetch('/token');
+      const {token} = await res.json();
+      return token;
+    },
+  },
   reducers: searchAppReducers,
 });
