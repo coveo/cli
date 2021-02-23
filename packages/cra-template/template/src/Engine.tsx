8fa64a10f1e03d5c410eae6facde49a87e860ba5
Yassine Lakhdar <ylakhdar@coveo.com>
2021-02-23 11:41:22 -0500

    flatten promise
    https://coveord.atlassian.net/browse/CDX-61

--- packages/cra-template/template/src/Engine.tsx
+++ packages/cra-template/template/src/Engine.tsx
@@ -5,11 +5,11 @@ export const headlessEngine = new HeadlessEngine({
     platformUrl: process.env.REACT_APP_PLATFORM_URL,
     organizationId: process.env.REACT_APP_ORGANIZATION_ID!,
     accessToken: process.env.REACT_APP_API_KEY!,
-    renewAccessToken: () =>
-      window
-        .fetch('/token')
-        .then((data) => data.json())
-        .then((data) => data.token),
+    renewAccessToken: async () => {
+      const res = await fetch('/token');
+      const {token} = await res.json();
+      return token;
+    },
   },
   reducers: searchAppReducers,
 });
