0e6f7af405513efeb55d4f72cf0f7a46104f731c
Yassine Lakhdar <ylakhdar@coveo.com>
2021-02-24 17:35:08 -0500

    add arguments to vue command
    https://coveord.atlassian.net/browse/CDX-39

--- packages/cli/src/commands/ui/create/vue.ts
+++ packages/cli/src/commands/ui/create/vue.ts
@@ -1,9 +1,13 @@
 import {Command, flags} from '@oclif/command';
-import {dirname, resolve} from 'path';
+import {resolve} from 'path';
 import {
   buildAnalyticsFailureHook,
   buildAnalyticsSuccessHook,
 } from '../../../hooks/analytics/analytics';
+import {Config} from '../../../lib/config/config';
+import AuthenticationRequired from '../../../lib/decorators/authenticationRequired';
+import {Storage} from '../../../lib/oauth/storage';
+import {platformUrl} from '../../../lib/platform/environment';
 import {spawnProcess} from '../../../lib/utils/process';
 
 export default class Vue extends Command {
@@ -19,7 +23,6 @@ export default class Vue extends Command {
         'If not specified, the default TypeScript preset will be taken.',
         'For more information about Vue CLI presets, please consult https://cli.vuejs.org/guide/plugins-and-presets.html#presets',
       ].join('\n'),
-      // default: TODO: add default preset once it is available in npm https://coveord.atlassian.net/browse/CDX-39
     }),
   };
 
@@ -32,6 +35,7 @@ export default class Vue extends Command {
     {name: 'name', description: 'application name', required: true},
   ];
 
+  @AuthenticationRequired()
   async run() {
     const {args, flags} = this.parse(Vue);
 
@@ -45,9 +49,7 @@ export default class Vue extends Command {
       }
     }
     await this.createProject(args.name, preset);
-    await this.installPlugin(args.name);
     await this.invokePlugin(args.name);
-    this.startServer(args.name);
     await this.config.runHook(
       'analytics',
       buildAnalyticsSuccessHook(this, flags)
@@ -63,35 +65,28 @@ export default class Vue extends Command {
     throw err;
   }
 
-  private installPlugin(applicationName: string) {
-    // TODO: DELETE THIS METHOD ONCE THE PLUGIN IS PUBLISHED AND PART OF THE PRESET
-    // CDX-39
-    // Once the coveo plugin is published to npm, simply include it in the preset typescript-preset.json
-    // This will prevent from running `2 npm install` commands (one by @vue/cli, one for the plugin)
-    const pathToPlugin = dirname(require.resolve('vue-cli-plugin-coveo'));
-    return spawnProcess(
-      'npm',
-      ['install', '--save-dev', `file:${pathToPlugin}`],
-      {
-        cwd: applicationName,
-      }
-    );
-  }
+  private async invokePlugin(applicationName: string) {
+    const cfg = await this.configuration.get();
+    const storage = await this.storage.get();
 
-  private invokePlugin(applicationName: string) {
-    return this.runVueCliCommand(['invoke', 'vue-cli-plugin-coveo'], {
+    const cliArgs = [
+      'invoke',
+      // '@coveo/typescript',
+      '@coveo/vue-cli-plugin-typescript',
+      '--orgId',
+      cfg.organization,
+      '--apiKey',
+      storage.accessToken!,
+      '--platformUrl',
+      platformUrl({environment: cfg.environment}),
+      // TODO: CDX-91 Extract user email from oauth flow
+      '--user',
+      'foo@acme.com',
+    ];
+
+    return this.runVueCliCommand(cliArgs, {
       cwd: applicationName,
     });
-  }
-
-  private startServer(applicationName: string) {
-    // TODO: DELETE THIS METHOD ONCE THE PLUGIN IS PUBLISHED AND PART OF THE PRESET
-    // The @vue/cli already logs instructions once the installation completes
-    this.log(`Successfully created project ${applicationName}.`);
-    this.log('Get started with the following commands:\n');
-
-    this.log('$ cd ${applicationName}');
-    this.log('$ yarn serve');
   }
 
   private createProject(name: string, preset: {}) {
@@ -100,11 +95,21 @@ export default class Vue extends Command {
       name,
       '--inlinePreset',
       JSON.stringify(preset),
+      '--skipGetStarted',
+      '--bare',
     ]);
   }
 
   private runVueCliCommand(args: string[], options = {}) {
     const executable = require.resolve('@vue/cli/bin/vue.js');
     return spawnProcess(executable, args, options);
+  }
+
+  private get configuration() {
+    return new Config(this.config.configDir, this.error);
+  }
+
+  private get storage() {
+    return new Storage();
   }
 }
