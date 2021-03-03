8dadda2486ad81cfd544e6e891d6c67caa9021b0
Yassine Lakhdar <ylakhdar@coveo.com>
2021-03-03 16:53:08 -0500

    more tests
    https://coveord.atlassian.net/browse/CDX-133

--- packages/cli-e2e/__tests__/vue.specs.ts
+++ packages/cli-e2e/__tests__/vue.specs.ts
@@ -7,60 +7,123 @@ import {CLI_EXEC_PATH, killCliProcess} from '../utils/cli';
 import {closeAllPages, getBrowser} from '../utils/browser';
 import axios from 'axios';
 
+declare const document: any;
+
 describe('ui', () => {
   describe('create:vue', () => {
     let browser: Browser;
     let cliProcess: ChildProcessWithoutNullStreams;
+    const clientPort = '8080'; // TODO: find port dynamically
+    const projectName = 'my-project';
+    const searchPageEndpoint = `http://localhost:${clientPort}`;
+    const tokenProxyEndpoint = `http://localhost:${clientPort}/token`;
 
     beforeAll(async () => {
       browser = await getBrowser();
-      // TODO: login()
+      // TODO: build the project
+      // TODO: Run the dev server
+      // cliProcess = spawn(CLI_EXEC_PATH, ['ui:create:vue', projectName]);
+      // return new Promise<void>((resolve) => {
+      //   getBrowser().then((b) => (browser = b));
+      //   cliProcess = spawn('npm', ['run', 'start'], {
+      //     // cliProcess = spawn('ls', ['-al'], {
+      //     cwd: projectName,
+      //   });
+      //   setTimeout(() => {
+      //     resolve();
+      //   }, 15e1);
+      // });
+      // setTimeout(() => {
+      //   done();
+      // }, 2000);
     });
 
-    afterEach(async () => {
-      const killCliPromise = killCliProcess(cliProcess);
-      const pageClosePromises = await closeAllPages(browser);
-      await Promise.all([killCliPromise, ...pageClosePromises]);
-    }, 5e3);
-
     afterAll(async () => {
+      // await killCliProcess(cliProcess);
       await browser.close();
     });
 
-    describe('App with default parameters', () => {
-      // TODO: find port dynamically
-      const clientPort = '8080';
-      const serverPort = '4000';
-      const projectName = 'my-project';
+    afterEach(async () => {
+      const pageClosePromises = await closeAllPages(browser);
+      await Promise.all(pageClosePromises);
+    }, 5e3);
 
-      beforeAll(() => {
-        // TODO: need new release of @coveo/vue-cli-plugin-typescript otherwise it wont work
-        // cliProcess = spawn(CLI_EXEC_PATH, ['ui:create:vue', projectName]);
-        cliProcess = spawn('npm', ['run', 'start'], {
-          cwd: projectName,
-        });
-        jest.setTimeout(10e3);
+    // it('should get a search token', async () => {
+    //   const response = await axios.get(`http://localhost:4000/token`);
+    //   expect(response.data).toMatchObject({
+    //     token: expect.stringMatching(/.+/),
+    //   });
+    // });
+
+    it('should contain a hero section', async () => {
+      const page = await browser.newPage();
+      await page.goto(searchPageEndpoint, {
+        waitUntil: 'networkidle2',
       });
+      const aHandle = await page.evaluateHandle(() =>
+        // TODO: declare document
+        document.querySelector('.hero h1')
+      );
 
-      it('should return a search search token', async () => {
-        const response = await axios.get(
-          `http://localhost:${serverPort}/token`
-        );
-        expect(response.data).toMatchObject({
-          token: expect.stringMatching(/.+/),
-        });
-      }, 30e3);
+      const resultHandle = await page.evaluateHandle(
+        (element) => element.innerHTML,
+        aHandle
+      );
 
-      it('should open a working search page', async () => {
-        const page1 = await browser.newPage();
-        await page1.goto(`http://localhost:${clientPort}`);
-        // TODO: make sure the page loads correctly
-        // TODO: make sure we are able to query the Search API
-      }, 30e3);
+      expect(await resultHandle.jsonValue()).toEqual(
+        'Welcome to Your Coveo Vue.js Search Page'
+      );
     });
 
-    it.todo('should create a Vue.js project with a custom preset');
-    it.todo('should create a Vue.js project with an invalid environment file');
-    // It should redirect the user to an error page
+    it('should contain a search page section', async () => {
+      const page = await browser.newPage();
+      await page.goto(searchPageEndpoint, {
+        waitUntil: 'networkidle2',
+      });
+
+      expect(await page.$('#search-page')).not.toBeNull();
+    });
+
+    it('should retrieve the search token on the page load', async () => {
+      const page = await browser.newPage();
+      page.goto(searchPageEndpoint);
+      const tokenResponse = await page.waitForResponse(tokenProxyEndpoint);
+      expect(JSON.parse(await tokenResponse.text())).toMatchObject({
+        token: expect.stringMatching(/.+/),
+      });
+    });
+
+    it('should trigger search queries', async () => {
+      const page = await browser.newPage();
+      const searchboxSelector = '#search-page .autocomplete input';
+      let isInterfaceLoadSearch = true;
+
+      page.on('request', (request) => {
+        if (
+          request.method() === 'POST' &&
+          request.url().indexOf(
+            // TODO: Test with Prod environment
+            'https://platformdev.cloud.coveo.com/rest/search/v2?organizationId'
+          ) === 0
+        ) {
+          const data = JSON.parse(request.postData());
+          if (!isInterfaceLoadSearch) {
+            expect(data).toMatchObject({
+              q: 'my query',
+            });
+          }
+          isInterfaceLoadSearch = false;
+        }
+      });
+
+      await page.goto(searchPageEndpoint, {waitUntil: 'networkidle2'});
+      await page.waitForSelector(searchboxSelector);
+      await page.focus(searchboxSelector);
+      await page.keyboard.type('my query');
+      await page.keyboard.press('Enter');
+    });
   });
+
+  it.todo('should create a Vue.js project with a custom preset');
+  it.todo('should redirect the user to an error page if invalid env file');
 });
