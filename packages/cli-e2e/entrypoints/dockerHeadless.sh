2e9e157b4e203587376e0085a51f1394bae0f9c8
Louis Bompart <lbompart@coveo.com>
2021-03-08 10:26:27 -0500

    test(cli): modify test env to be keyring-compatible
    https://coveord.atlassian.net/browse/CDX-142

--- packages/cli-e2e/entrypoints/dockerHeadless.sh
+++ packages/cli-e2e/entrypoints/dockerHeadless.sh
@@ -3,12 +3,10 @@ Xvfb :1 -screen 0 1024x768x16 & sleep 1
 
 xdg-settings set default-web-browser google-chrome.desktop
 
-useradd -ms /bin/bash notGroot
-rsync -r --exclude="node_modules" /home/cli/* /home/cli-copy
-chmod -R 777 /home/cli-copy
-cd /home/cli-copy
+rsync -r --exclude="node_modules" /home/notGroot/cli/* /home/notGroot/cli-copy/
+cd /home/notGroot/cli-copy
 
-sudo -u notGroot npm run setup
+npm run setup
 cd packages/cli-e2e
-sudo -u notGroot google-chrome --no-first-run --remote-debugging-port=9222 --disable-dev-shm-usage >/dev/null 2>&1 & \
-sudo -u notGroot npm run-script jest
+google-chrome --no-first-run --remote-debugging-port=9222 --disable-dev-shm-usage >/dev/null 2>&1 & \
+npm run-script jest:debug
