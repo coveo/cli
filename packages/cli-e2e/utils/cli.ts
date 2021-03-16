e959b0ac4d54356f3ad88d59e1bd33212452d452
Yassine <ylakhdar@coveo.com>
2021-03-16 10:02:59 -0400

    test(cli): add E2E tests for ui:create:vue (#69)
    https://coveord.atlassian.net/browse/CDX-133

--- packages/cli-e2e/utils/cli.ts
+++ packages/cli-e2e/utils/cli.ts
@@ -1,5 +1,7 @@
-import type {ChildProcessWithoutNullStreams} from 'child_process';
+import stripAnsi from 'strip-ansi';
+import {ChildProcessWithoutNullStreams, spawn} from 'child_process';
 import {resolve} from 'path';
+import {EOL} from 'os';
 
 export function killCliProcess(cliProcess: ChildProcessWithoutNullStreams) {
   const waitForKill = new Promise<void>((resolve) => {
@@ -9,8 +11,28 @@ export function killCliProcess(cliProcess: ChildProcessWithoutNullStreams) {
   return waitForKill;
 }
 
+export function killCliProcessFamily(
+  cliProcessLeader: ChildProcessWithoutNullStreams
+) {
+  const waitForKill = new Promise<void>((resolve) => {
+    cliProcessLeader.on('close', () => resolve());
+  });
+  process.kill(-cliProcessLeader.pid);
+  return waitForKill;
+}
+
 export function isYesNoPrompt(data: string) {
   return data.trimEnd().toLowerCase().endsWith('(y/n):');
+}
+
+export function isGenericYesNoPrompt(data: string) {
+  let stripedData = data;
+  try {
+    stripedData = stripAnsi(data.toString());
+  } catch (error) {
+    console.log('Unable to strip ansi from string', error);
+  }
+  return stripedData.match(/\(y\/n\)[\s:]*$/i) !== null;
 }
 
 export function answerPrompt(
@@ -25,4 +47,46 @@ export function answerPrompt(
     }
   });
 }
+
+export async function setupUIProject(
+  commandArgs: string,
+  projectName: string,
+  cliProcesses: ChildProcessWithoutNullStreams[]
+) {
+  const createProjectPromise = new Promise<void>((resolve) => {
+    const buildProcess = spawn(CLI_EXEC_PATH, [commandArgs, projectName]);
+
+    buildProcess.stdout.on('close', async () => {
+      resolve();
+    });
+    buildProcess.stdout.on('data', async (data) => {
+      if (isGenericYesNoPrompt(data.toString())) {
+        await answerPrompt(`y${EOL}`, buildProcess);
+      }
+    });
+  });
+
+  await createProjectPromise;
+
+  return new Promise<void>((resolve) => {
+    const startServerProcess = spawn('npm', ['run', 'start'], {
+      cwd: projectName,
+      detached: true,
+    });
+
+    cliProcesses.push(startServerProcess);
+    setTimeout(() => {
+      resolve();
+    }, 15e3);
+  });
+}
+
+export async function teardownUIProject(
+  cliProcesses: ChildProcessWithoutNullStreams[]
+) {
+  return Promise.all(
+    cliProcesses.map((cliProcess) => killCliProcessFamily(cliProcess))
+  );
+}
+
 export const CLI_EXEC_PATH = resolve(__dirname, '../../cli/bin/run');
