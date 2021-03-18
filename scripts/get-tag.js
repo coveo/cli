314bd4c45165c8a33ea9af2f4726d53d04ca8ef9
Louis Bompart <lbompart@coveo.com>
2021-03-18 17:45:30 -0400

    ci(githubactions): improve tag acquisition on build binaries (#99)
    https://coveord.atlassian.net/browse/CDX-172

---
+++ scripts/get-tag.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {getLatestTag} = require('./github-client');
+
+const tagRegExp = /refs\/tags\/(v[\d.]+)/;
+
+const getTag = async () => {
+  if (github.context.eventName === 'workflow_dispatch') {
+    return github.context.payload.inputs.version;
+  }
+  if (github.context.eventName === 'release') {
+    const ref = github.context.ref;
+    const match = tagRegExp.exec(ref);
+    if (match && match[1]) {
+      return match[1];
+    }
+  }
+  core.warning('Tag acquisition failed, fallbacking to the latest');
+  return await getLatestTag();
+};
+
+(async () => {
+  try {
+    const tag = await getTag();
+    core.exportVariable('tag', tag);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
