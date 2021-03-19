a1721e13cb1aedb078590ddb52452cc25253fe0a
Yassine Lakhdar <ylakhdar@coveo.com>
2021-03-19 10:07:56 -0400

    add react e2e tests
    https://coveord.atlassian.net/browse/CDX-173

---
+++ packages/cli-e2e/__tests__/react.specs.ts
@@ -0,0 +1,78 @@
+import retry from 'async-retry';
+
+import type {ChildProcessWithoutNullStreams} from 'child_process';
+import type {HTTPRequest, Browser, Page} from 'puppeteer';
+
+import {setupUIProject, teardownUIProject} from '../utils/cli';
+import {getNewBrowser} from '../utils/browser';
+import {isSearchRequest} from '../utils/platform';
+
+describe('ui', () => {
+  describe('create:react', () => {
+    let browser: Browser;
+    const cliProcesses: ChildProcessWithoutNullStreams[] = [];
+    // TODO: CDX-90: Assign a dynamic port for the search token server on all ui projects
+    const clientPort = '3000';
+    const projectName = 'react-project';
+    const searchPageEndpoint = `http://localhost:${clientPort}`;
+    const tokenProxyEndpoint = `http://localhost:${clientPort}/token`;
+    let interceptedRequests: HTTPRequest[] = [];
+    let page: Page;
+
+    beforeAll(async () => {
+      browser = await getNewBrowser();
+      await setupUIProject('ui:create:react', projectName, cliProcesses, {
+        timeout: 30e3,
+      });
+    }, 3e6);
+
+    beforeEach(async () => {
+      page = await browser.newPage();
+
+      page.on('request', (request: HTTPRequest) => {
+        interceptedRequests.push(request);
+      });
+    });
+
+    afterAll(async () => {
+      await browser.close();
+      await teardownUIProject(cliProcesses);
+    }, 5e3);
+
+    it('should contain a search page section', async () => {
+      await page.goto(searchPageEndpoint, {
+        waitUntil: 'networkidle2',
+      });
+
+      expect(await page.$('div.App')).not.toBeNull();
+    });
+
+    it('should retrieve the search token on the page load', async () => {
+      page.goto(searchPageEndpoint);
+      const tokenResponse = await page.waitForResponse(tokenProxyEndpoint);
+      expect(JSON.parse(await tokenResponse.text())).toMatchObject({
+        token: expect.stringMatching(/^eyJhb.+/),
+      });
+    });
+
+    it('should trigger search queries', async () => {
+      const searchboxSelector = 'div.App .MuiAutocomplete-root input';
+      await page.goto(searchPageEndpoint, {waitUntil: 'networkidle2'});
+
+      // Request from interface load
+      expect(interceptedRequests.some(isSearchRequest)).toBeTruthy();
+      interceptedRequests = [];
+
+      await page.waitForSelector(searchboxSelector);
+      await page.focus(searchboxSelector);
+      await page.keyboard.type('my query');
+      await page.keyboard.press('Enter');
+
+      await retry(async () => {
+        expect(interceptedRequests.some(isSearchRequest)).toBeTruthy();
+      });
+    });
+  });
+
+  it.todo('should redirect the user to an error page if invalid env file');
+});
