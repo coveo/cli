50f699cac450f49c77f44bce590f0ab19d9cc559
Yassine <ylakhdar@coveo.com>
2021-03-25 13:41:01 -0400

    feat(cli): add --version flag for all ui:create:* commands (#105)
    https://coveord.atlassian.net/browse/CDX-188

---
+++ packages/cli/src/lib/utils/misc.ts
@@ -0,0 +1,15 @@
+import {resolve} from 'path';
+import {coerce} from 'semver';
+
+export function getPackageVersion(packageName: string) {
+  const pathToPackageJson = resolve(
+    __dirname,
+    ...new Array(3).fill('..'),
+    'package.json'
+  );
+  const pkg = require(pathToPackageJson);
+  const dep = pkg.dependencies[packageName] || pkg.devDependencies[packageName];
+  const defaultVersion = coerce(dep.toString())?.version;
+
+  return defaultVersion;
+}
