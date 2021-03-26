c4b74e75c075b8da082ce0f740ae358177dafebc
Louis Bompart <lbompart@coveo.com>
2021-03-26 14:09:10 -0400

    fix(angular): fix search-token-server path (#115)
    https://coveord.atlassian.net/browse/CDX-192

--- packages/angular/src/search-token-server/rules/templates.ts
+++ packages/angular/src/search-token-server/rules/templates.ts
@@ -3,9 +3,9 @@ import {CoveoSchema} from '../../schema';
 import {dirname} from 'path';
 
 export function createServerDirectory(options: CoveoSchema) {
-  const searchTokenServeTemplate = dirname(
+  const searchTokenServeTemplate = `file://${dirname(
     require.resolve('@coveo/search-token-server')
-  );
+  )}`;
 
   return createFiles(options, './server', searchTokenServeTemplate);
 }
