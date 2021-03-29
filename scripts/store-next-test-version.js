6537fe85c0a53d993235e5a2bf4ca94e1ccf47ad
Yassine Lakhdar <ylakhdar@coveo.com>
2021-03-29 16:34:44 -0400

    update deploy scripts
    https://coveord.atlassian.net/browse/CDX-190

--- scripts/store-next-test-version.js
+++ scripts/store-next-test-version.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-undef */
+const {spawnSync} = require('child_process');
+const yargs = require('yargs/yargs');
+const {hideBin} = require('yargs/helpers');
 const {inc, valid, gte} = require('semver');
 const {
   getUiTemplates,
@@ -17,14 +20,32 @@ function getGreatestVersion(versions = []) {
   return validVersions.reduce(reducer);
 }
 
-async function main() {
+async function getNextTestVersion() {
   const templates = getUiTemplates();
   const allNextVersions = await Promise.all(
     templates.map((p) => getPackageNextTestVersion(p))
   );
 
   const greatestVersion = getGreatestVersion(allNextVersions);
-  console.log(greatestVersion);
+  return greatestVersion;
+}
+
+function saveValueInEnvVariable(variable, value) {
+  if (Boolean(variable) === false) {
+    console.error(
+      'Please specify an environment variable to store the next test version'
+    );
+    return;
+  }
+  spawnSync('echo', [`"${variable}=${value}"`, '>>', '$GITHUB_ENV'], {
+    stdio: 'inherit',
+  });
+}
+
+async function main() {
+  const argv = yargs(hideBin(process.argv)).argv;
+  const nextTestVersion = await getNextTestVersion();
+  saveValueInEnvVariable(argv.variable, nextTestVersion);
 }
 
 main();
