a6399af6dee27d946d4364c5dfbc5a6765087f2d
Louis Bompart <lbompart@coveo.com>
2021-04-21 16:46:25 -0400

    ci(release): copy binary to 'latest'
    https://coveord.atlassian.net/browse/CDX-275

---
+++ scripts/copy-latest-binaries.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const binariesMatcher = /^coveo(_|-)(?<_version>v?\d+\.\d+\.\d+(-\d)?)(?<longExt>.*\.(exe|deb|pkg))$/;
+const distDir = 'dist';
+const getOsSlug = () => {
+  switch (process.platform) {
+    case 'win32':
+      return 'win';
+    case 'darwin':
+      return 'macos';
+    case 'linux':
+      return 'deb';
+  }
+};
+const getBinaryFolder = () => path.resolve(distDir, getOsSlug());
+
+const files = fs.readdirSync(getBinaryFolder(), {withFileTypes: true});
+files.forEach((file) => {
+  const match = binariesMatcher.exec(file.name);
+  if (!match) {
+    return;
+  }
+  const destName = `coveo-latest${match.groups.longExt}`;
+  fs.copyFileSync(
+    path.resolve(getBinaryFolder(), file.name),
+    path.resolve(distDir, destName)
+  );
+});
