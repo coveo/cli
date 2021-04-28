65f4e1b16f05fd6c0def4d6751723c555e7fefd1
Yassine Lakhdar <ylakhdar@coveo.com>
2021-04-28 15:31:02 -0400

    move declaration in dedicated file
    https://coveord.atlassian.net/browse/CDX-236

--- packages/search-token-server/app.ts
+++ packages/search-token-server/app.ts
@@ -1,18 +1,9 @@
-/* eslint-disable @typescript-eslint/no-namespace */
 import * as express from 'express';
 import {ensureTokenGenerated} from './middlewares/searchToken';
 import {errorHandler} from './middlewares/errorHandler';
 import {environmentCheck} from './middlewares/environmentCheck';
 
 const app = express();
-
-declare global {
-  namespace Express {
-    interface Request {
-      token: string;
-    }
-  }
-}
 
 app.use(express.json());
 
