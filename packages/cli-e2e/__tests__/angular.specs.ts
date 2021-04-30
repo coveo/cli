16b79ef7ac0dca30b0685b2e1cebe7ae1d7c5a9a
Yassine Lakhdar <ylakhdar@coveo.com>
2021-04-30 15:37:03 -0400

    update angular e2e tests
    https://coveord.atlassian.net/browse/CDX-235

--- packages/cli-e2e/__tests__/angular.specs.ts
+++ packages/cli-e2e/__tests__/angular.specs.ts
@@ -13,92 +13,128 @@ import {isSearchRequest} from '../utils/platform';
 import {EOL} from 'os';
 import stripAnsi from 'strip-ansi';
 import {ProcessManager} from '../utils/processManager';
+import {join} from 'path';
+import {renameSync} from 'fs-extra';
 
-describe.skip('ui', () => {
-  describe('create:angular', () => {
-    let browser: Browser;
-    let processManager: ProcessManager;
-    // TODO: CDX-90: Assign a dynamic port for the search token server on all ui projects
-    const clientPort = '4200';
-    const projectName = `${process.env.GITHUB_ACTION}-angular-project`;
-    const searchPageEndpoint = `http://localhost:${clientPort}`;
-    const tokenProxyEndpoint = `http://localhost:${clientPort}/token`;
-    let interceptedRequests: HTTPRequest[] = [];
-    let page: Page;
+describe('ui:create:angular', () => {
+  let browser: Browser;
+  let buildProcessManager: ProcessManager;
+  let page: Page;
+  const oldEnv = process.env;
+  const projectName = `${process.env.GITHUB_ACTION}-angular-project`;
+  // TODO: CDX-90: Assign a dynamic port for the search token server on all ui projects
+  const clientPort = '4200';
+  const searchPageEndpoint = `http://localhost:${clientPort}`;
+  const tokenProxyEndpoint = `http://localhost:${clientPort}/token`;
 
-    const searchboxSelector = 'app-search-page app-search-box input';
+  const buildApplication = async (processManager: ProcessManager) => {
+    const proc = setupUIProject(
+      processManager,
+      'ui:create:angular',
+      projectName,
+      {
+        flags: ['--defaults'],
+      }
+    );
 
-    beforeAll(async () => {
-      processManager = new ProcessManager();
-      browser = await getNewBrowser();
-      const buildProcess = setupUIProject(
-        processManager,
-        'ui:create:angular',
-        projectName,
-        {
-          flags: ['--defaults'],
-        }
-      );
+    proc.stdout.on('data', async (data) => {
+      if (isGenericYesNoPrompt(data.toString())) {
+        await answerPrompt(`y${EOL}`, proc);
+      }
+    });
 
-      buildProcess.stdout.on('data', async (data) => {
-        if (isGenericYesNoPrompt(data.toString())) {
-          await answerPrompt(`y${EOL}`, buildProcess);
-        }
-      });
-
-      await Promise.race([
-        new Promise<void>((resolve) => {
-          buildProcess.on('exit', async () => {
-            resolve();
-          });
-        }),
-        new Promise<void>((resolve) => {
-          buildProcess.stdout.on('data', (data) => {
-            if (
-              /Happy hacking !/.test(
-                stripAnsi(data.toString()).replace(/\n/g, '')
-              )
-            ) {
-              resolve();
-            }
-          });
-        }),
-      ]);
-
-      const startServerProcess = processManager.spawn('npm', ['run', 'start'], {
-        cwd: getProjectPath(projectName),
-      });
-
-      await new Promise<void>((resolve) => {
-        startServerProcess.stdout.on('data', async (data) => {
+    return Promise.race([
+      new Promise<void>((resolve) => {
+        proc.on('exit', async () => {
+          resolve();
+        });
+      }),
+      new Promise<void>((resolve) => {
+        proc.stdout.on('data', (data) => {
           if (
-            /Compiled successfully/.test(
+            /Happy hacking !/.test(
               stripAnsi(data.toString()).replace(/\n/g, '')
             )
           ) {
             resolve();
           }
         });
+      }),
+    ]);
+  };
+
+  const startApplication = async (processManager: ProcessManager) => {
+    const proc = processManager.spawn('npm', ['run', 'start'], {
+      cwd: getProjectPath(projectName),
+    });
+
+    return new Promise<void>((resolve) => {
+      proc.stdout.on('data', async (data) => {
+        if (
+          /Compiled successfully/.test(
+            stripAnsi(data.toString()).replace(/\n/g, '')
+          )
+        ) {
+          resolve();
+        }
       });
-    }, 420e3);
+    });
+  };
+
+  const deactivateEnvironmentFile = () => {
+    const pathToEnv = getProjectPath(projectName);
+    renameSync(join(pathToEnv, '.env'), join(pathToEnv, '.env.disabled'));
+  };
+
+  const restoreEnvironmentFile = () => {
+    const pathToEnv = getProjectPath(projectName);
+    renameSync(join(pathToEnv, '.env.disabled'), join(pathToEnv, '.env'));
+  };
+
+  beforeAll(async () => {
+    buildProcessManager = new ProcessManager();
+    browser = await getNewBrowser();
+    await buildApplication(buildProcessManager);
+  }, 7 * 60e3);
+
+  beforeEach(async () => {
+    jest.resetModules();
+    process.env = {...oldEnv};
+    page = await openNewPage(browser, page);
+  });
+
+  afterEach(async () => {
+    await captureScreenshots(browser);
+  });
+
+  afterAll(async () => {
+    process.env = oldEnv;
+    await buildProcessManager.killAllProcesses();
+    await browser.close();
+  });
+
+  describe('when the project is configured correctly', () => {
+    let serverProcessManager: ProcessManager;
+    let interceptedRequests: HTTPRequest[] = [];
+    const searchboxSelector = 'app-search-page app-search-box input';
+    beforeAll(async () => {
+      serverProcessManager = new ProcessManager();
+      await startApplication(serverProcessManager);
+    }, 60e3);
 
     beforeEach(async () => {
-      page = await openNewPage(browser, page);
-
       page.on('request', (request: HTTPRequest) => {
         interceptedRequests.push(request);
       });
     });
 
     afterEach(async () => {
-      await captureScreenshots(browser);
       page.removeAllListeners('request');
       interceptedRequests = [];
     });
 
     afterAll(async () => {
-      await browser.close();
-      await processManager.killAllProcesses();
+      await serverProcessManager.killAllProcesses();
     }, 5e3);
 
     it('should contain a search page section', async () => {
@@ -146,5 +182,24 @@ describe.skip('ui', () => {
     });
   });
 
-  it.todo('should redirect the user to an error page if invalid env file');
+  describe('when the required environment variables are missing', () => {
+    let serverProcessManager: ProcessManager;
+    const errorPage = `http://localhost:${clientPort}/error`;
+
+    beforeAll(async () => {
+      serverProcessManager = new ProcessManager();
+      await deactivateEnvironmentFile();
+      await startApplication(serverProcessManager);
+    }, 60e3);
+
+    afterAll(async () => {
+      await restoreEnvironmentFile();
+      await serverProcessManager.killAllProcesses();
+    }, 5e3);
+
+    it('should redirect the user to an error page', async () => {
+      await page.goto(searchPageEndpoint, {waitUntil: 'networkidle2'});
+      expect(page.url()).toEqual(errorPage);
+    });
+  });
 });
