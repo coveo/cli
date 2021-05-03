cecedf9cc2287742f2d35b45a8533c32a0563c57
Olivier Lamothe <olamothe@coveo.com>
2021-05-03 14:46:40 -0400

    cleanup
    https://coveord.atlassian.net/browse/CDX-281

--- scripts/inject-headless-version.js
+++ scripts/inject-headless-version.js
@@ -1,11 +1,36 @@
-import {readFileSync} from 'fs';
+const {readFileSync, writeFileSync} = require('fs');
+
+const packages = ['angular', 'cra-template', 'vue-cli-plugin-typescript'];
+const headlessVersion = '0.x';
+
 async function main() {
-  const packages = ['angular', 'cra-template', 'vue-cli-plugin-typescript'];
-  //  const headlessVersion = '0.x';
   packages.forEach((p) => {
-    const f = readFileSync(`./packages/${p}/package.json`);
-    console.log(f);
+    let packageJSON = JSON.parse(
+      readFileSync(`./packages/${p}/package.json`).toString()
+    );
+    packageJSON = replaceInDevDependencies(packageJSON, headlessVersion);
+    packageJSON = replaceInDependencies(packageJSON, headlessVersion);
+    writeFileSync(
+      `./packages/${p}/package.json`,
+      JSON.stringify(packageJSON, null, 4) + '\n'
+    );
   });
 }
+
+const replaceInDevDependencies = (packageJSON, versionToReplace) => {
+  return replace(packageJSON, versionToReplace, 'devDependencies');
+};
+
+const replaceInDependencies = (packageJSON, versionToReplace) => {
+  return replace(packageJSON, versionToReplace, 'dependencies');
+};
+
+const replace = (packageJSON, versionToReplace, section) => {
+  if (packageJSON[section] && packageJSON[section]['@coveo/headless']) {
+    packageJSON[section]['@coveo/headless'] = versionToReplace;
+  }
+
+  return packageJSON;
+};
 
 main();
