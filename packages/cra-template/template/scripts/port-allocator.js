d635ebca734daa72545fb64dedb66a4121f328da
Yassine Lakhdar <ylakhdar@coveo.com>
2021-05-18 10:08:33 -0400

    add dynamic port to react project
    https://coveord.atlassian.net/browse/CDX-99

---
+++ packages/cra-template/template/scripts/port-allocator.js
@@ -0,0 +1,47 @@
+const getPort = require('get-port');
+const {appendFileSync, truncateSync} = require('fs');
+const {config} = require('dotenv');
+const {EOL} = require('os');
+const environment = config();
+
+const preferedWebAppPort = 3000;
+const portRangeFallback = [3000, 3999];
+
+const updateEnvFile = () => {
+  const env = {
+    ...environment.parsed,
+    PORT: process.env.PORT,
+    REACT_APP_SERVER_PORT: process.env.REACT_APP_SERVER_PORT,
+  };
+
+  truncateSync('.env');
+  for (const [key, value] of Object.entries(env)) {
+    appendFileSync('.env', `${key}=${value}${EOL}`);
+  }
+};
+
+const allocatePorts = async () => {
+  process.env.PORT = await getNextAvailablePorts({
+    port: process.env.PORT || preferedWebAppPort,
+  });
+  process.env.REACT_APP_SERVER_PORT = await getNextAvailablePorts({
+    port: process.env.REACT_APP_SERVER_PORT,
+  });
+  updateEnvFile();
+};
+
+const getNextAvailablePorts = async (preferedPort) => {
+  preferedPort = isNaN(parseInt(preferedPort)) ? null : parseInt(preferedPort);
+
+  if (await isPortAvailable(preferedPort)) {
+    return preferedPort;
+  }
+  return await getPort({port: getPort.makeRange(...portRangeFallback)});
+};
+
+const isPortAvailable = async (port) => {
+  const availablePort = await getPort({port});
+  return availablePort === port;
+};
+
+allocatePorts();
--- packages/cra-template/template/src/common/Engine.tsx
+++ packages/cra-template/template/src/common/Engine.tsx
@@ -1,7 +1,20 @@
 import {HeadlessEngine, searchAppReducers} from '@coveo/headless';
 
+const getEndpointToLocalServer = () => {
+  if (!process.env.REACT_APP_SERVER_PORT) {
+    throw new Error('Undefined "REACT_APP_SERVER_PORT" environment variable');
+  }
+  const port = process.env.REACT_APP_SERVER_PORT;
+  const pathname = '/token';
+  return `http://localhost:${port}${[pathname]}`;
+};
+
+const getTokenEndpoint = () => {
+  return process.env.REACT_APP_TOKEN_ENDPOINT || getEndpointToLocalServer();
+};
+
 export async function getSearchToken() {
-  const res = await fetch(process.env.REACT_APP_TOKEN_ENDPOINT!);
+  const res = await fetch(getTokenEndpoint());
   const {token} = await res.json();
   return token;
 }
