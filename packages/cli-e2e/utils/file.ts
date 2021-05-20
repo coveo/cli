ba4cfb02ce5fc28bc9a4e64a2d447d625777787f
Yassine Lakhdar <ylakhdar@coveo.com>
2021-05-20 15:33:40 -0400

    add additional methods to manipulate .env file
    https://coveord.atlassian.net/browse/CDX-99

--- packages/cli-e2e/utils/file.ts
+++ packages/cli-e2e/utils/file.ts
@@ -1,9 +1,15 @@
-import {renameSync} from 'fs';
+import {
+  renameSync,
+  existsSync,
+  readFileSync,
+  writeFileSync,
+  truncateSync,
+} from 'fs';
 import {join} from 'path';
 import {getProjectPath} from './cli';
 
-const deactivatedEnvFileName = '.env.disabled';
-const activeEnvFilename = '.env';
+export const deactivatedEnvFileName = '.env.disabled';
+export const activeEnvFilename = '.env';
 
 function swapEnv(projectName: string, frm: string, to: string) {
   const pathToEnv = getProjectPath(projectName);
@@ -16,4 +22,28 @@ export function deactivateEnvironmentFile(projectName: string) {
 
 export function restoreEnvironmentFile(projectName: string) {
   swapEnv(projectName, deactivatedEnvFileName, activeEnvFilename);
+}
+
+export function flushEnvFile(projectName: string) {
+  const envPath = join(getProjectPath(projectName), '.env');
+  const env = readFileSync(envPath, 'utf-8');
+  truncateSync(envPath);
+  return env;
+}
+
+export function overwriteEnvFile(projectName: string, data: string) {
+  const envPath = join(getProjectPath(projectName), '.env');
+  writeFileSync(envPath, data);
+}
+
+export function isEnvFileActive(projectName: string) {
+  const projectPath = getProjectPath(projectName);
+  return existsSync(join(projectPath, '.env'));
+}
+
+export function getPathToEnvFile(projectName: string) {
+  const projectPath = getProjectPath(projectName);
+  return isEnvFileActive(projectName)
+    ? join(projectPath, activeEnvFilename)
+    : join(projectPath, deactivatedEnvFileName);
 }
