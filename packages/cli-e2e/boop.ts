86ddadc7bfac3cb2dec85d50bd2b3ce67fbd91b2
Louis Bompart <lbompart@coveo.com>
2021-05-20 15:34:34 -0400

    yep

--- packages/cli-e2e/boop.ts
+++ packages/cli-e2e/boop.ts
@@ -1,13 +1,16 @@
-import setup from './setup';
-import path from 'path';
+import {resolve} from 'path';
+import {Terminal} from './utils/terminal/terminal';
+
 (async () => {
-  console.log('boop');
-  //https://github.com/oclif/config/blob/1066a42a61a71b9a0708a2beff498d2cbbb9a3fd/src/config.ts#L455
-  console.log(
-    (process.env.HOMEDRIVE &&
-      process.env.HOMEPATH &&
-      path.join(process.env.HOMEDRIVE!, process.env.HOMEPATH!)) ||
-      process.env.USERPROFILE
-  );
-  await setup();
+  await new Terminal(
+    resolve(__dirname, '..', 'cli', 'bin', 'run.cmd'),
+    ['help'],
+    undefined,
+    undefined,
+    'foobar'
+  )
+    .when(/.+/)
+    .on('stdout')
+    .do()
+    .once();
 })();
