863f897c188db853d64ba0da70c6997b275ed654
Louis Bompart <lbompart@coveo.com>
2021-05-20 14:10:27 -0400

    but why no homedir bruh

--- packages/cli-e2e/boop.ts
+++ packages/cli-e2e/boop.ts
@@ -1,6 +1,13 @@
 import setup from './setup';
-
+import path from 'path';
 (async () => {
   console.log('boop');
+  //https://github.com/oclif/config/blob/1066a42a61a71b9a0708a2beff498d2cbbb9a3fd/src/config.ts#L455
+  console.log(
+    (process.env.HOMEDRIVE &&
+      process.env.HOMEPATH &&
+      path.join(process.env.HOMEDRIVE!, process.env.HOMEPATH!)) ||
+      process.env.USERPROFILE
+  );
   await setup();
 })();
