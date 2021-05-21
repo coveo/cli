acbc6e6a0b1528352b06ccf527b6aea150c0a122
Yassine Lakhdar <ylakhdar@coveo.com>
2021-05-21 13:54:26 -0400

    add E2E tests for Angular
    https://coveord.atlassian.net/browse/CDX-99

--- packages/angular/src/search-token-server/files/scripts/port-allocator.js
+++ packages/angular/src/search-token-server/files/scripts/port-allocator.js
@@ -3,6 +3,7 @@ const {
   readFileSync,
   writeFileSync,
   truncateSync,
+  existsSync,
   appendFileSync,
 } = require('fs');
 const {EOL} = require('os');
@@ -13,6 +14,14 @@ const environment = config({path: pathToServerEnv}).parsed;
 
 const preferedWebAppPort = 4200;
 const portRangeFallback = [4000, 4999];
+
+const ensureEnvironmentFile = () => {
+  if (!existsSync(pathToServerEnv)) {
+    throw new Error(
+      '.env file not found in the project root. Refer to the README to for more information for more information.'
+    );
+  }
+};
 
 const updateTokenServerEnvironment = (serverPort) => {
   const env = {
@@ -41,36 +50,39 @@ const addTokenServerPortToWebApp = async (serverPort) => {
 };
 
 const allocatePorts = async () => {
-  const appPort = await getNextAvailablePorts(preferedWebAppPort);
+  const appPort = await getNextAvailablePorts(preferedWebAppPort, {
+    host: '127.0.0.1',
+  });
   const serverPort = await getNextAvailablePorts(environment.SERVER_PORT);
+
   return [appPort, serverPort];
 };
 
-const getNextAvailablePorts = async (preferedPort) => {
+const getNextAvailablePorts = async (preferedPort, getPortOptions) => {
   if (typeof preferedPort !== 'number') {
     preferedPort = isNaN(parseInt(preferedPort))
       ? null
       : parseInt(preferedPort);
   }
 
-  if (await isPortAvailable(preferedPort)) {
+  if (await isPortAvailable(preferedPort, getPortOptions)) {
     return preferedPort;
   }
   return await getPort({
     port: getPort.makeRange(...portRangeFallback),
-    host: '127.0.0.1',
+    ...getPortOptions,
   });
 };
 
-const isPortAvailable = async (port) => {
-  const availablePort = await getPort({port, host: '127.0.0.1'});
+const isPortAvailable = async (port, options) => {
+  const availablePort = await getPort({port, ...options});
   return availablePort === port;
 };
 
 function updateAppPort(port) {
   const angularJsonPath = resolve('angular.json');
 
-  const angularJSON = JSON.parse(readFileSync(angularJsonPath));
+  const angularJSON = JSON.parse(readFileSync(angularJsonPath, 'utf-8'));
   const projectName = angularJSON.defaultProject;
   const serveOptions =
     angularJSON.projects[projectName].architect.serve.options;
@@ -80,6 +92,7 @@ function updateAppPort(port) {
 }
 
 async function main() {
+  ensureEnvironmentFile();
   const [appPort, serverPort] = await allocatePorts();
   await addTokenServerPortToWebApp(serverPort);
   updateTokenServerEnvironment(serverPort);
