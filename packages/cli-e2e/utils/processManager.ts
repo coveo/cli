06eb6be3b6e5d4f6b40a532f6bbafea4dae19e3e
Louis Bompart <lbompart@coveo.com>
2021-05-25 14:41:24 -0400

    some configs

--- packages/cli-e2e/utils/processManager.ts
+++ packages/cli-e2e/utils/processManager.ts
@@ -43,7 +43,7 @@ export class ProcessManager {
               console.error(
                 `Process pid is not a number. Received pid: ${currentProcess.pid}`
               );
-              resolve();
+              return resolve();
             }
             process.kill(
               (process.platform === 'win32' ? +1 : -1) * currentProcess.pid
