be2810c0336f9f2e6565f77b0c9bd34c99acc860
Yassine Lakhdar <ylakhdar@coveo.com>
2021-05-30 10:55:50 -0400

    fix conflicts
    https://coveord.atlassian.net/browse/CDX-353

---
+++ packages/cli/src/lib/project/project.spec.ts
@@ -0,0 +1,109 @@
+jest.mock('@coveord/platform-client');
+jest.mock('fs');
+jest.mock('archiver');
+
+import {mocked} from 'ts-jest/utils';
+import {existsSync, createWriteStream, WriteStream, unlinkSync} from 'fs';
+import {Project} from './project';
+import {join} from 'path';
+import * as archiver from 'archiver';
+import {Writable} from 'stream';
+const mockedExistSync = mocked(existsSync);
+const mockedUnlinkSync = mocked(unlinkSync);
+const mockedCreateWriteStream = mocked(createWriteStream);
+const mockedArchiver = mocked(archiver);
+const mockedPipe = jest.fn();
+const mockedPassDirectory = jest.fn();
+const mockedFinalize = jest.fn();
+
+mockedArchiver.mockImplementation(
+  () =>
+    ({
+      on: () => {},
+      pipe: mockedPipe,
+      directory: mockedPassDirectory,
+      finalize: mockedFinalize,
+    } as unknown as archiver.Archiver)
+);
+
+const doMockValidProject = () => {
+  mockedExistSync.mockReturnValue(true);
+};
+
+const doMockInValidProject = () => {
+  mockedExistSync.mockReturnValue(false);
+};
+
+const doMockCreateWriteStream = () => {
+  mockedCreateWriteStream.mockImplementation(() => {
+    const writableStream = new Writable();
+    writableStream._write = (_chunk, _encoding, next) => next();
+    process.nextTick(() => writableStream.emit('close'));
+    return writableStream as unknown as WriteStream;
+  });
+};
+
+describe('Project', () => {
+  doMockCreateWriteStream();
+  const projectCreator = () => new Project('dummy/path');
+
+  beforeEach(() => {
+    doMockInValidProject();
+  });
+
+  describe('if the project is invalid', () => {
+    it('should throw an error', () => {
+      expect(projectCreator).toThrowError(/Invalid Project/);
+    });
+
+    it('should ensure project exists', async () => {
+      expect(projectCreator).toThrow();
+      expect(mockedExistSync).toHaveBeenCalledWith(
+        join('dummy', 'path', 'resources')
+      );
+    });
+  });
+
+  describe('if the project is valid', () => {
+    let pathToResources: string;
+    let pathToZip: string;
+    let project: Project;
+
+    beforeEach(async () => {
+      doMockValidProject();
+      pathToResources = join('dummy', 'path', 'resources');
+      pathToZip = join('dummy', 'path', 'snapshot.zip');
+      project = projectCreator();
+      await project.compressResources();
+    });
+
+    it('should no throw an error', () => {
+      expect(projectCreator).not.toThrowError();
+    });
+
+    it('#compressResources should create a write stream the appropriate path to the zip file', () => {
+      expect(mockedCreateWriteStream).toHaveBeenCalledWith(pathToZip);
+    });
+
+    it('#compressResources should append files from the resource directory', () => {
+      const putTheContentAtTheRootOfTheArchive = false;
+      expect(mockedPassDirectory).toHaveBeenCalledWith(
+        pathToResources,
+        putTheContentAtTheRootOfTheArchive
+      );
+    });
+
+    it('#compressResources should finalize when there are no more resources to zip', () => {
+      expect(mockedFinalize).toHaveBeenCalledTimes(1);
+    });
+
+    it('#deleteTemporaryZipFile should delete the temporary a zip file', () => {
+      project.deleteTemporaryZipFile();
+      expect(mockedUnlinkSync).toHaveBeenCalledWith(pathToZip);
+    });
+
+    it.todo(
+      '#compressResources should reject if the archiver returns an error'
+    );
+  });
+});
---
+++ packages/cli/src/lib/project/project.ts
@@ -0,0 +1,57 @@
+import {createWriteStream, existsSync, unlinkSync} from 'fs';
+import {join} from 'path';
+import {cli} from 'cli-ux';
+import * as archiver from 'archiver';
+
+export class Project {
+  constructor(private pathToProject: string) {
+    this.ensureProjectCompliance();
+  }
+
+  private get pathToTemporaryZip() {
+    return join(this.pathToProject, 'snapshot.zip');
+  }
+
+  private get pathToResources() {
+    return join(this.pathToProject, 'resources');
+  }
+
+  deleteTemporaryZipFile() {
+    unlinkSync(this.pathToTemporaryZip);
+  }
+
+  ensureProjectCompliance() {
+    /*
+     * TODO: CDX-354: add checks to ensure the project is indeed a valid project
+     * e.g. * Check if path to resources is a folder
+     *      * Check if the root has a valid config file
+     */
+
+    if (!existsSync(this.pathToResources)) {
+      throw new Error('Invalid Project. TODO: better error message');
+    }
+  }
+
+  async compressResources() {
+    const archivePromise = () =>
+      new Promise<void>((resolve, reject) => {
+        const pathToTemporaryZip = this.pathToTemporaryZip;
+        const outputStream = createWriteStream(pathToTemporaryZip);
+        const archive = archiver('zip');
+
+        outputStream.on('close', () => resolve());
+        archive.on('error', (err) => reject(err));
+
+        archive.pipe(outputStream);
+        archive.directory(this.pathToResources, false);
+        archive.finalize();
+      });
+
+    try {
+      await archivePromise();
+      return this.pathToTemporaryZip;
+    } catch (error) {
+      cli.error(error);
+    }
+  }
+}
