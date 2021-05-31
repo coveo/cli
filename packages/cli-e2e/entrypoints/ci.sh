b1269eb9f62987e6d0b94516294012bbee6b028c
Louis Bompart <lbompart@coveo.com>
2021-05-31 18:36:00 -0400

    npm i g
    https://coveord.atlassian.net/browse/CDX-368

--- packages/cli-e2e/entrypoints/ci.sh
+++ packages/cli-e2e/entrypoints/ci.sh
@@ -19,9 +19,9 @@ yarn config set -- --install.silent true
 yarn config set -- --silent true
 
 npm run npm:bump:template -- -- $UI_TEMPLATE_VERSION
-
-sudo npx verdaccio --config packages/cli-e2e/docker/config/config.yaml &
-while ! timeout 1 bash -c "echo > /dev/tcp/localhost/4873"; do sleep 10; done
+npm i -g verdaccio
+verdaccio --config packages/cli-e2e/docker/config/config.yaml
+# while ! timeout 1 bash -c "echo > /dev/tcp/localhost/4873"; do sleep 10; done
 
 npm run npm:publish:template
 cd packages/cli-e2e
