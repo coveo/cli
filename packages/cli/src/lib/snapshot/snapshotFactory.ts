c7a11095766babb2a3f28d801415cdc0b9a9eef5
Yassine Lakhdar <ylakhdar@coveo.com>
2021-05-31 10:41:54 -0400

    set static
    https://coveord.atlassian.net/browse/CDX-360

--- packages/cli/src/lib/snapshot/snapshotFactory.ts
+++ packages/cli/src/lib/snapshot/snapshotFactory.ts
@@ -1,14 +1,14 @@
 import {Snapshot} from './snapshot';
 
 export class SnapshotFactory {
-  async createFromZip(
+  static async createFromZip(
     pathToZip: string,
     developerNotes: string
   ): Promise<Snapshot> {
     throw new Error('TODO: CDX-353: Create Zip from project');
   }
 
-  async createFromOrg() {
+  static async createFromOrg() {
     // TODO:
   }
 }
