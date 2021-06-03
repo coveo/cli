7b8e2fe00b27a515fc2aab76e00cba92b2bd708a
Yassine Lakhdar <ylakhdar@coveo.com>
2021-06-03 18:23:48 -0400

    some basic reporting
    https://coveord.atlassian.net/browse/CDX-346

--- packages/cli/src/lib/snapshot/report.ts
+++ packages/cli/src/lib/snapshot/report.ts
@@ -3,6 +3,7 @@ import {
   ResourceSnapshotsReportResultCode,
 } from '@coveord/platform-client';
 import {cli} from 'cli-ux';
+import {bgHex} from 'chalk';
 
 /**
  * TODO: Remove this 2 interfaces
@@ -23,27 +24,86 @@ export interface ResourceSnapshotsReportOperations {
   [resourceType: string]: ResourceSnapshotsReportOperationModel;
 }
 
+// TODO: rename class
 export class Report {
+  private colors = {
+    warn: '#DC8D44',
+    error: '#F64D64',
+  };
+
   public constructor(private report: ResourceSnapshotsReportModel) {}
 
   public preview(): void {
-    this.printTitle();
+    cli.log('Previewing changes');
+    cli.log('');
 
+    this.printTable();
+
+    cli.log('');
+    cli.log('Info');
     if (!this.isSuccessReport()) {
       this.handleReportErrors();
     }
-    // this.printTable();
+    this.printSummary();
   }
 
-  private printTitle() {
-    cli.log('Content summary');
+  private printSummary() {
+    const created = this.getOperationTypeCount('resourcesCreated');
+    const updated = this.getOperationTypeCount('resourcesUpdated');
+    const deleted = this.getOperationTypeCount('resourcesDeleted');
+    if (created) {
+      cli.log(`${created} resources to create`);
+    }
+    if (updated) {
+      cli.log(`${updated} resources to update`);
+    }
+    if (deleted) {
+      cli.log(`${deleted} resources to delete`);
+    }
   }
 
   private printTable() {
-    throw new Error('');
+    const resources = this.getAffectedResources();
+    cli.table(resources, {
+      resourceName: {
+        header: 'Resource',
+        get: (row) => `x ${row.resourceName}`,
+      },
+      operations: {header: 'Operations'},
+    });
   }
 
-  private getAffectedResources() {}
+  // TODO: rename and use getter
+  private getAffectedResources() {
+    const resourceOperations: ResourceSnapshotsReportOperations = JSON.parse(
+      JSON.stringify(this.report.resourceOperations)
+    );
+
+    const resourceHasAtLeastOneOperation = ([_, operations]: [
+      string,
+      ResourceSnapshotsReportOperationModel
+    ]) => {
+      const sumReducer = (a: number, b: number) => a + b;
+      const allResourceOperations = Object.values(operations);
+      const numberOfAffectedResources = allResourceOperations.reduce(
+        sumReducer,
+        0
+      );
+      return numberOfAffectedResources > 0;
+    };
+
+    const convertArrayToObject = ([resourceName, operations]: [
+      string,
+      ResourceSnapshotsReportOperationModel
+    ]) => ({
+      resourceName,
+      operations,
+    });
+
+    return Object.entries(resourceOperations)
+      .filter(resourceHasAtLeastOneOperation)
+      .map(convertArrayToObject);
+  }
 
   private prettyPrintResourceName(resourceName: string): string {
     throw new Error('TODO');
@@ -57,21 +117,28 @@ export class Report {
     return this.report.resultCode === ResourceSnapshotsReportResultCode.Success;
   }
 
-  private get totalNumberOfErrors() {
+  private getOperationTypeCount(
+    type: keyof ResourceSnapshotsReportOperationModel
+  ) {
     const operations = this.report
       .resourceOperations as ResourceSnapshotsReportOperations;
 
     const errorCount = Object.values(operations).reduce(
       (count: number, current: ResourceSnapshotsReportOperationModel) =>
-        count + current.resourcesInError,
+        count + current[type],
       0
     );
 
     return errorCount;
   }
 
+  private error(message: string) {
+    cli.log(bgHex(this.colors.error).hex('#272C3A')(message));
+  }
+
   private handleReportErrors() {
-    cli.warn(`${this.totalNumberOfErrors} resources in error`);
+    const errorCount = this.getOperationTypeCount('resourcesInError');
+    this.error(`${errorCount} resources in error`.toUpperCase());
     // TODO: CDX-362: handle other invalid snashot cases
     // cli.log(`${this.totalNumberOfErrors}`);
   }
