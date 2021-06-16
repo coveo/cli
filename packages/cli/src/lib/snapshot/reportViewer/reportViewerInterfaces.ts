389b1f882f93b1534393fb9de6cbb4b49e08f843
Yassine Lakhdar <ylakhdar@coveo.com>
2021-06-16 14:53:11 -0400

    remove old file
    https://coveord.atlassian.net/browse/CDX-361

--- packages/cli/src/lib/snapshot/reportViewer/reportViewerInterfaces.ts
+++
@@ -1,9 +0,0 @@
-import {ResourceSnapshotsReportOperationModel} from '@coveord/platform-client';
-
-export type ReportViewerOperationName =
-  keyof ResourceSnapshotsReportOperationModel;
-
-export interface ReportViewerResourceReportModel {
-  name: string;
-  operations: ResourceSnapshotsReportOperationModel;
-}
