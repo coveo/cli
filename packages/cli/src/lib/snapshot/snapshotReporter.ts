a55da2472aabf418dd5b9a5433c8c51c3097dd05
Yassine Lakhdar <ylakhdar@coveo.com>
2021-06-23 16:59:48 -0400

    moving logic to reportViewer
    https://coveord.atlassian.net/browse/CDX-412

---
+++ packages/cli/src/lib/snapshot/snapshotReporter.ts
@@ -0,0 +1,70 @@
+import {
+  ResourceSnapshotsReportModel,
+  ResourceSnapshotsReportOperationModel,
+  ResourceSnapshotsReportResultCode,
+  ResourceSnapshotsReportStatus,
+} from '@coveord/platform-client';
+import {
+  ReportViewerOperationName,
+  ReportViewerResourceReportModel,
+} from './reportViewer/reportViewerDataModels';
+
+export class SnapshotReporter {
+  public constructor(private readonly report: ResourceSnapshotsReportModel) {}
+
+  public get resourceOperationResults() {
+    return this.report.resourceOperationResults;
+  }
+
+  public hasChangedResources() {
+    const totalUnchanges =
+      this.getOperationTypeTotalCount('resourcesUnchanged');
+    return totalUnchanges === this.report.resourcesProcessed;
+  }
+
+  public getOperationTypeTotalCount(
+    type: keyof ResourceSnapshotsReportOperationModel
+  ) {
+    const count = Object.values(this.report.resourceOperations).reduce(
+      (count: number, current: ResourceSnapshotsReportOperationModel) =>
+        count + current[type],
+      0
+    );
+
+    return count;
+  }
+
+  public isSuccessReport(): boolean {
+    const {status, resultCode} = this.report;
+    return (
+      status === ResourceSnapshotsReportStatus.Completed &&
+      resultCode === ResourceSnapshotsReportResultCode.Success
+    );
+  }
+
+  public getChangedResources(
+    operationsToDisplay: ReportViewerOperationName[]
+  ): ReportViewerResourceReportModel[] {
+    type resourceEntries = [string, ResourceSnapshotsReportOperationModel];
+    const resourceHasAtLeastOneOperation = ([
+      _,
+      operations,
+    ]: resourceEntries) => {
+      return (
+        operationsToDisplay.reduce(
+          (previous, current) => previous + operations[current],
+          0
+        ) > 0
+      );
+    };
+
+    const convertArrayToObject = ([name, operations]: resourceEntries) => ({
+      name,
+      operations,
+    });
+
+    return Object.entries(this.report.resourceOperations)
+      .filter(resourceHasAtLeastOneOperation)
+      .map(convertArrayToObject);
+  }
+}
