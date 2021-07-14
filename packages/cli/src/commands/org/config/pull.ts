696222153e9b27fc333d8225501970137949f6d5
Yassine <ylakhdar@coveo.com>
2021-07-14 09:10:58 -0400

    Update packages/cli/src/commands/org/config/pull.ts
    Co-authored-by: jpmarceau <39384459+jpmarceau@users.noreply.github.com>

--- packages/cli/src/commands/org/config/pull.ts
+++ packages/cli/src/commands/org/config/pull.ts
@@ -77,7 +77,7 @@ export default class Pull extends Command {
       cli.info(
         dedent`
 
-          Once the snapshot is created, you can pull it with the following command
+          Once the snapshot is created, you can pull it with the following command:
 
               ${blueBright`coveo org:config:pull ${snapshot.id}`}`
       );
