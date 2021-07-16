37b2b9c18791d1451db3fac3d6a695a1ca5003cf
Yassine <ylakhdar@coveo.com>
2021-07-16 14:40:34 -0400

    feat(cli): add `--resourceTypes` flag (#341)
    https://coveord.atlassian.net/browse/CDX-447

--- packages/cli/src/commands/org/config/pull.ts
+++ packages/cli/src/commands/org/config/pull.ts
@@ -1,5 +1,6 @@
 import {ResourceType} from '@coveord/platform-client';
 import {flags, Command} from '@oclif/command';
+import {IOptionFlag} from '@oclif/command/lib/flags';
 import {cwd} from 'process';
 import {Config} from '../../../lib/config/config';
 import {
@@ -17,12 +18,19 @@ export default class Pull extends Command {
   public static flags = {
     target: flags.string({
       char: 't',
-      description:
-        'The unique identifier of the organization from which to pull the resources. If not specified, the organization you are connected to will be used.',
       helpValue: 'destinationorganizationg7dg3gd',
       required: false,
+      description:
+        'The unique identifier of the organization from which to pull the resources. If not specified, the organization you are connected to will be used.',
     }),
-    // TODO: CDX-447: add flag to select resource types to export
+    resourceTypes: flags.string({
+      char: 'r',
+      helpValue: 'type1 type2',
+      options: Object.keys(ResourceType),
+      default: Object.keys(ResourceType),
+      multiple: true,
+      description: 'The resources types to pull from the organization.',
+    }) as IOptionFlag<(keyof typeof ResourceType)[]>,
   };
 
   public static hidden = true;
@@ -30,7 +38,6 @@ export default class Pull extends Command {
   @Preconditions(IsAuthenticated())
   public async run() {
     const snapshot = await this.getSnapshot();
-
     await this.refreshProject(snapshot);
     await snapshot.delete();
   }
@@ -51,8 +58,9 @@ export default class Pull extends Command {
     return new Config(this.config.configDir, this.error);
   }
 
-  private get resourceTypesToExport(): ResourceType[] {
-    throw new Error('not implemented yet');
+  private get resourceTypesToExport() {
+    const {flags} = this.parse(Pull);
+    return flags.resourceTypes.map((type) => ResourceType[type]);
   }
 
   private get projectPath() {
