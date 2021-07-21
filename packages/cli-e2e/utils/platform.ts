a7cc86f8b24ff5033d1ea41aa991a72c95b0d280
Yassine Lakhdar <ylakhdar@coveo.com>
2021-07-21 10:16:52 -0400

    finally, delete the test org
    https://coveord.atlassian.net/browse/CDX-467

--- packages/cli-e2e/utils/platform.ts
+++ packages/cli-e2e/utils/platform.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import {HTTPRequest} from 'puppeteer';
 
+// TODO: CDX-98: URL should vary in function of the target environment.
+export const platformHost = 'https://platformdev.cloud.coveo.com/rest/';
+
 export function isSearchRequest(request: HTTPRequest) {
-  return request.url().startsWith(`${hostname()}search/v2?organizationId`);
+  return request.url().startsWith(`${platformHost}search/v2?organizationId`);
 }
 
 export async function createOrg(
@@ -11,17 +14,12 @@ export async function createOrg(
   organizationTemplate = 'Developer'
 ): Promise<string> {
   const request = await axios.post(
-    `${hostname()}organizations?name=${name}&organizationTemplate=${organizationTemplate}`,
+    `${platformHost}organizations?name=${name}&organizationTemplate=${organizationTemplate}`,
     {},
     authHeader(accessToken)
   );
 
   return request.data.id;
-}
-
-function hostname() {
-  // TODO: CDX-98: URL should vary in function of the target environment.
-  return 'https://platformdev.cloud.coveo.com/rest/';
 }
 
 function authHeader(accessToken: string) {
--- scripts/delete-org.js
+++ scripts/delete-org.js
@@ -1,28 +1,29 @@
-const {config} = require('dotenv');
+const {axios} = require('axios');
 const {homedir} = require('os');
 const {join} = require('path');
+const {config} = require('dotenv');
 config({path: join(homedir(), '.env')});
 
-// function authHeader(accessToken) {
-//   return {
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`,
-//     },
-//   };
-// }
+// TODO: CDX-98: URL should vary in function of the target environment.
+const platformHost = 'https://platformdev.cloud.coveo.com/rest/';
 
-async function deleteTestOrg(orgId, _accessToken) {
+function authHeader(accessToken) {
+  return {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+}
+
+async function deleteTestOrg(orgId, accessToken) {
   if (orgId === 'connectorsteamtestsmf76kcam') {
     throw new Error('Au bûcher! Au bûcher!');
   }
-  console.log('*********************');
-  console.log(`deleteting org: ${orgId}`);
-  console.log('*********************');
 
-  // await axios.delete(
-  //   `${hostname()}organizations/${orgId}`,
-  //   authHeader(accessToken)
-  // );
+  await axios.delete(
+    `${platformHost}organizations/${orgId}`,
+    authHeader(accessToken)
+  );
 }
 
 async function main() {
