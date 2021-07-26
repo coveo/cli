410951a75b12b64cf305a77abbcbe1d9d145df34
Yassine Lakhdar <ylakhdar@coveo.com>
2021-07-26 15:05:09 -0400

    fix merge conflict
    https://coveord.atlassian.net/browse/CDX-508

--- packages/cli/src/commands/org/config/pull.ts
+++ packages/cli/src/commands/org/config/pull.ts
@@ -63,7 +63,7 @@ export default class Pull extends Command {
   public async catch(err?: Error) {
     const {flags} = this.parse(Pull);
     handleSnapshotError(err);
-    this.displayAdditionalErrorMessage(err);
+    await this.displayAdditionalErrorMessage(err);
     await this.config.runHook(
       'analytics',
       buildAnalyticsFailureHook(this, flags, err)
