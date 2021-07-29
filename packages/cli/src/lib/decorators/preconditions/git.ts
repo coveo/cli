55090bd4eb3a44427f9056a1fd559b1c63dfc969
Louis Bompart <lbompart@coveo.com>
2021-07-29 14:17:55 -0400

    lint&prettier
    https://coveord.atlassian.net/browse/CDX-516

--- packages/cli/src/lib/decorators/preconditions/git.ts
+++ packages/cli/src/lib/decorators/preconditions/git.ts
@@ -1,11 +1,7 @@
-import {appendCmdIfWindows} from '../../utils/os';
 import {getBinInstalledPrecondition} from './binPreconditionsFactory';
 
-export const IsGitInstalled = getBinInstalledPrecondition(
-  'git',
-  {
-    prettyName: 'git',
-    howToInstallBinText:
-      'https://git-scm.com/book/en/v2/Getting-Started-Installing-Git',
-  }
-);
+export const IsGitInstalled = getBinInstalledPrecondition('git', {
+  prettyName: 'git',
+  howToInstallBinText:
+    'https://git-scm.com/book/en/v2/Getting-Started-Installing-Git',
+});
