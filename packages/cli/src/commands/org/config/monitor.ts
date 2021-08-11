38b64f3de76d4ddd16b5e9c1b4dfd412c46b1634
Yassine Lakhdar <ylakhdar@coveo.com>
2021-08-11 16:05:09 -0400

    merge conflict
    https://coveord.atlassian.net/browse/CDX-529

--- packages/cli/src/commands/org/config/monitor.ts
+++ packages/cli/src/commands/org/config/monitor.ts
@@ -58,7 +58,7 @@ export default class Monitor extends Command {
 
   public async catch(err?: Error) {
     const {flags} = this.parse(Monitor);
-    handleSnapshotError(this.projectPath, err);
+    handleSnapshotError(err);
     await this.config.runHook(
       'analytics',
       buildAnalyticsFailureHook(this, flags, err)
