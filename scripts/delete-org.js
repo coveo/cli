026e177a5a76f71ac084ed3def39571628f8af9c
Yassine Lakhdar <ylakhdar@coveo.com>
2021-10-04 16:14:22 -0400

    troubleshooting tests
    https://coveord.atlassian.net/browse/CDX-567

--- scripts/delete-org.js
+++ scripts/delete-org.js
@@ -1,31 +1,31 @@
-const axios = require('axios');
+// const axios = require('axios');
 const {homedir} = require('os');
 const {join} = require('path');
 const {config} = require('dotenv');
 config({path: join(homedir(), '.env')});
 
 // TODO: CDX-98: URL should vary in function of the target environment.
-const platformHost = 'https://platformdev.cloud.coveo.com/rest/';
+// const platformHost = 'https://platformdev.cloud.coveo.com/rest/';
 
-function authHeader(accessToken) {
-  return {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  };
-}
+// function authHeader(accessToken) {
+//   return {
+//     headers: {
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//   };
+// }
 
-async function deleteTestOrg(orgId, accessToken) {
+async function deleteTestOrg(orgId, _accessToken) {
   if (orgId === 'connectorsteamtestsmf76kcam') {
     throw new Error('Au bûcher! Au bûcher!');
   }
 
   if (orgId) {
     console.log(`Deleting org ${orgId}`);
-    await axios.delete(
-      `${platformHost}organizations/${orgId}`,
-      authHeader(accessToken)
-    );
+    // await axios.delete(
+    //   `${platformHost}organizations/${orgId}`,
+    //   authHeader(accessToken)
+    // );
   } else {
     console.log('No org to delete');
   }
