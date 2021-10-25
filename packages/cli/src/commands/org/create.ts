50aee3c928e2501878113d5aa33eedaeb28e7261
Yassine <ylakhdar@coveo.com>
2021-10-25 11:28:36 -0400

    feat: add `coveo:org:create` command (#509)
    https://coveord.atlassian.net/browse/CDX-282

---
+++ packages/cli/src/commands/org/create.ts
@@ -0,0 +1,96 @@
+import {Command, flags} from '@oclif/command';
+import {AuthenticatedClient} from '../../lib/platform/authenticatedClient';
+import {cli} from 'cli-ux';
+import {
+  buildAnalyticsFailureHook,
+  buildAnalyticsSuccessHook,
+} from '../../hooks/analytics/analytics';
+import {
+  Preconditions,
+  IsAuthenticated,
+} from '../../lib/decorators/preconditions/';
+import {OrganizationCreationOrigin} from '@coveord/platform-client';
+import {Config} from '../../lib/config/config';
+import {bold} from 'chalk';
+import dedent from 'ts-dedent';
+
+export default class Create extends Command {
+  public static description = 'Create a new test Coveo organization.';
+
+  public static flags = {
+    setDefaultOrganization: flags.boolean({
+      char: 's',
+      default: true,
+      allowNo: true,
+      description: 'Set the created organization as the default one',
+    }),
+  };
+
+  public static args = [
+    {
+      name: 'name',
+      helpValue: 'neworg-prod',
+      description: 'The name to assign to the new organization.',
+      required: true,
+    },
+  ];
+
+  @Preconditions(IsAuthenticated())
+  public async run() {
+    cli.action.start('Creating organization');
+    const {id} = await this.createOrganization();
+    const endMessage = await this.generateEndMessageFromOrgId(id);
+    cli.action.stop(endMessage);
+
+    await this.config.runHook(
+      'analytics',
+      buildAnalyticsSuccessHook(this, flags)
+    );
+  }
+
+  public async catch(err?: Error) {
+    const {flags} = this.parse(Create);
+    await this.config.runHook(
+      'analytics',
+      buildAnalyticsFailureHook(this, flags, err)
+    );
+    throw err;
+  }
+
+  private async createOrganization() {
+    const {args} = this.parse(Create);
+    const client = await new AuthenticatedClient().getClient();
+    return client.organization.create({
+      creationOrigin: OrganizationCreationOrigin.CLI,
+      name: args.name,
+      organizationTemplate: 'Developer',
+    });
+  }
+
+  private async generateEndMessageFromOrgId(orgId: string) {
+    const {flags} = this.parse(Create);
+    const color1 = bold.cyan;
+    let color2 = bold.magenta;
+    if (flags.setDefaultOrganization) {
+      this.configuration.set('organization', orgId);
+      color2 = bold.cyan;
+    }
+
+    const cfg = await this.configuration.get();
+    const message = dedent`Organization ${color1(orgId)} successfully created.
+
+    You are currently logged into organization ${color2(
+      cfg.organization
+    )} and further command will be ran against that organization by default.
+
+    If you wish to switch organization use \`coveo config:set\`.
+
+    To see current configuration use \`coveo config:get\``;
+
+    return message;
+  }
+
+  private get configuration() {
+    return new Config(this.config.configDir, this.error);
+  }
+}
