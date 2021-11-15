f6cc75f3176872cc5847d3d092385223d71706b3
ylakhdar <ylakhdar@coveo.com>
2021-11-15 08:58:41 -0500

    exit with right code
    https://coveord.atlassian.net/browse/CDX-661

--- packages/cli/src/hooks/init/termination-signals.ts
+++ packages/cli/src/hooks/init/termination-signals.ts
@@ -1,16 +1,20 @@
 import {buildEvent} from '../analytics/eventUtils';
 
-export const signals = ['SIGINT', 'SIGTERM', 'SIGQUIT', 'SIGHUP'] as const;
+export const signalExits = ['SIGINT', 'SIGTERM', 'SIGQUIT', 'SIGHUP'] as const;
 
 export const handleTerminationSignals = () => {
-  signals.forEach((signal) => process.on(signal, () => trackEvent(signal)));
+  signalExits.forEach((signal) =>
+    process.on(signal, (_sig: string, code: number) => {
+      trackEvent(signal, code);
+    })
+  );
 };
 
-function trackEvent(termination_signal: string) {
+function trackEvent(termination_signal: string, exitCode: number) {
   config.runHook('analytics', {
     event: buildEvent('interrupted operation', {termination_signal}),
   });
 
   // eslint-disable-next-line no-process-exit
-  process.exit(0);
+  process.exit(128 + exitCode);
 }
