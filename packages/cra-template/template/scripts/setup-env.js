134074055ff28dd528cbe8834243a771d70a8513
y-lakhdar <ylakhdar@coveo.com>
2021-12-23 11:02:18 -0500

    ignore compilation warning
    https://coveord.atlassian.net/browse/CDX-736

--- packages/cra-template/template/scripts/setup-env.js
+++ packages/cra-template/template/scripts/setup-env.js
@@ -8,7 +8,8 @@ function createEnvFile(filePath) {
 REACT_APP_PLATFORM_URL=${platformUrl}
 REACT_APP_ORGANIZATION_ID=${orgId}
 REACT_APP_API_KEY=${apiKey}
-REACT_APP_USER_EMAIL=${user}`;
+REACT_APP_USER_EMAIL=${user}
+GENERATE_SOURCEMAP=false`; // TODO: CDX-737: fix exponential-backoff compilation warnings
 
   writeFileSync(filePath, projectEnvContent);
 }
