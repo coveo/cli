3d3328d4c10011e701bfa60a14e6acf539441810
y-lakhdar <ylakhdar@coveo.com>
2021-12-23 17:38:21 -0500

    fix clean-up.js
    https://coveord.atlassian.net/browse/CDX-736

--- packages/cra-template/template/scripts/clean-up.js
+++ packages/cra-template/template/scripts/clean-up.js
@@ -1,15 +1,12 @@
+const {spawnSync} = require('child_process');
 const {unlinkSync, readFileSync, writeFileSync} = require('fs');
 const {join} = require('path');
 const {getPackageManager} = require('./utils');
 const pkgJson = JSON.parse(readFileSync('package.json'));
 
-[
-  'prestart',
-  'setup-env',
-  'setup-server',
-  'setup-cleanup',
-  'commit-server',
-].forEach((script) => delete pkgJson['scripts'][script]);
+['prestart', 'setup-env', 'setup-server', 'setup-cleanup'].forEach(
+  (script) => delete pkgJson['scripts'][script]
+);
 
 const pkgString = JSON.stringify(pkgJson, null, 2).replace(
   /npm/g,
@@ -21,3 +18,8 @@ writeFileSync('package.json', pkgString);
 ['setup-server.js', 'setup-env.js', 'clean-up.js'].forEach((file) =>
   unlinkSync(join(__dirname, file))
 );
+
+spawnSync('git', ['add', '-A']);
+spawnSync('git', ['commit', '-m', 'initialize coveo react search page'], {
+  stdio: ['pipe', 'pipe', 'inherit'],
+});
