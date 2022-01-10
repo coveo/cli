598702f03fa9cb07049a403afb5087efcda6b1d9
y-lakhdar <ylakhdar@coveo.com>
2022-01-10 12:45:33 -0500

    document generator method
    https://coveord.atlassian.net/browse/CDX-715

--- packages/cli/src/lib/utils/iterator.ts
+++ packages/cli/src/lib/utils/iterator.ts
@@ -1,3 +1,5 @@
+type PromiseGenerator<T> = Generator<Promise<T>, void, unknown>;
+
 export const consumeIterator = async <T>(
   iterator: IterableIterator<T>,
   work: (item: T) => Promise<void>,
@@ -14,21 +16,49 @@ export const consumeIterator = async <T>(
   return Promise.allSettled(workers);
 };
 
-export const consumeGenerator = async (
-  generator: () => Generator<Promise<void>, void, unknown>,
-  maxConcurrent: number
+export const createWorkers = <T>(
+  generator: () => PromiseGenerator<T>,
+  maxConcurrent: number,
+  doWork: (
+    generator: PromiseGenerator<T>,
+    workerIndex: number
+  ) => Promise<T | void>
+) => {
+  return new Array(maxConcurrent)
+    .fill(generator())
+    .map((gen: PromiseGenerator<T>, workerIndex: number) =>
+      doWork(gen, workerIndex)
+    );
+};
+
+/**
+ * Consumes a generator function concurrently
+ *
+ * @template T
+ * @param {() => PromiseGenerator<T>} generator The generator function to consume.
+ * @param {number} maxConcurrent The number of maximum concurrent workers.
+ * @param {(worker: number, promiseValue: Awaited<T>) => void} [callback] A callback function that runs after each iteration of the iterator. It takes in argument the promise resolved value as well as the worker (ID) assigned to fulfill that promise.
+ */
+export const consumeGenerator = async <T>(
+  generator: () => PromiseGenerator<T>,
+  maxConcurrent: number,
+  callback?: (worker: number, promiseValue: Awaited<T>) => void
 ) => {
   const doWork = async (
-    generator: Generator<Promise<void>, void, unknown>
-  ): Promise<void> => {
+    generator: PromiseGenerator<T>,
+    workerIndex: number
+  ): Promise<T | void> => {
     const next = generator.next();
     if (next.done) {
       return;
     }
-    await next.value;
-    return doWork(generator);
+    const value = await next.value;
+    if (callback) {
+      callback(workerIndex, value);
+    }
+    return doWork(generator, workerIndex);
   };
-  const workers = new Array(maxConcurrent).fill(generator()).map(doWork);
 
+  const workers = createWorkers(generator, maxConcurrent, doWork);
   return Promise.all(workers);
 };
