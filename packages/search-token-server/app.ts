ccee5702801f6a4b8df28b9af5ea371bebb96b32
Louis Bompart <lbompart@coveo.com>
2022-01-20 16:39:32 -0500

    add keys
    https://coveord.atlassian.net/browse/CDX-773

--- packages/search-token-server/app.ts
+++ packages/search-token-server/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(cookieSession());
+app.use(cookieSession({keys: ['key1', 'key2']}));
 app.use(csurf());
 app.use(helmet());
 
