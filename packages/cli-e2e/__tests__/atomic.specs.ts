bec68f4ab4f61c69924927b3047285b555638f41
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-01-28 08:23:05 -0500

    hmm
    https://coveord.atlassian.net/browse/CDX-766

--- packages/cli-e2e/__tests__/atomic.specs.ts
+++ packages/cli-e2e/__tests__/atomic.specs.ts
@@ -6,6 +6,7 @@ import {ProcessManager} from '../utils/processManager';
 import {Terminal} from '../utils/terminal/terminal';
 import {BrowserConsoleInterceptor} from '../utils/browserConsoleInterceptor';
 import {npm} from '../utils/windows';
+import {jwtTokenPattern} from '../utils/matcher';
 import {EOL} from 'os';
 
 describe('ui:create:atomic', () => {
@@ -15,6 +16,7 @@ describe('ui:create:atomic', () => {
   const oldEnv = process.env;
   const projectName = `${process.env.TEST_RUN_ID}-atomic-project`;
   const searchPageEndpoint = 'http://localhost:8888';
+  const tokenServerEndpoint = 'http://localhost:8888/.netlify/functions/token';
 
   const waitForAppRunning = (appTerminal: Terminal) =>
     appTerminal
@@ -141,21 +143,6 @@ describe('ui:create:atomic', () => {
       expect(await page.$(searchInterfaceSelector)).not.toBeNull();
     }, 60e3);
 
-    // it('should retrieve the search token on the page load', async () => {
-    //   await page.goto(searchPageEndpoint, {waitUntil: 'networkidle2'});
-    //   await page.waitForSelector(searchInterfaceSelector);
-
-    //   const isTokenRequest = (request: HTTPRequest) => {
-    //     const tokenUrl = new URL(
-    //       '/.netlify/functions/token',
-    //       'http://localhost:8888'
-    //     );
-    //     return request.url().startsWith(tokenUrl.href);
-    //   };
-
-    //   expect(interceptedRequests.some(isTokenRequest)).toBeTruthy();
-    // }, 60e3);
-
     it('should send a search query when the page is loaded', async () => {
       await page.goto(searchPageEndpoint, {waitUntil: 'networkidle2'});
       await page.waitForSelector(searchInterfaceSelector);
@@ -164,6 +151,19 @@ describe('ui:create:atomic', () => {
       console.log('searchRequest', searchRequest);
 
       expect(searchRequest).toBeTruthy();
+    }, 60e3);
+
+    it('should retrieve the search token on the page load', async () => {
+      const tokenResponseListener = page.waitForResponse(tokenServerEndpoint);
+
+      page.goto(searchPageEndpoint);
+      await page.waitForSelector(searchInterfaceSelector);
+
+      expect(
+        JSON.parse(await (await tokenResponseListener).text())
+      ).toMatchObject({
+        token: expect.stringMatching(jwtTokenPattern),
+      });
     }, 60e3);
   });
 });
