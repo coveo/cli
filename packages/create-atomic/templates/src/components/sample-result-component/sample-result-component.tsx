c174e0aaa99353c5d82f898d9f369718f83183e5
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-02-01 11:26:50 -0500

    pr fixes
    https://coveord.atlassian.net/browse/CDX-766

--- packages/create-atomic/templates/src/components/sample-result-component/sample-result-component.tsx
+++ packages/create-atomic/templates/src/components/sample-result-component/sample-result-component.tsx
@@ -1,9 +1,13 @@
 import {Result} from '@coveo/atomic/headless';
-import {Component, h, Element} from '@stencil/core';
+import {Component, h, Element, State} from '@stencil/core';
 import {resultContext} from '@coveo/atomic';
 
 /**
  * Sample custom Atomic result component, to be used inside an Atomic Result Template.
+ *
+ * This component showcase a component that conditionally render the author of a result, with a fallback to display anonymous in the event that no author is available for a document, for educational purpose.
+ *
+ * In a real life scenario, we recommend you use https://docs.coveo.com/en/atomic/latest/reference/result-template-components/atomic-field-condition/ and https://docs.coveo.com/en/atomic/latest/reference/result-template-components/atomic-result-text/
  */
 @Component({
   tag: 'sample-result-component',
@@ -11,10 +15,12 @@ import {resultContext} from '@coveo/atomic';
   shadow: true,
 })
 export class SampleResultComponent {
-  private result?: Result;
+  // The Headless result object to be resolved from the parent atomic-result component.
+  @State() private result?: Result;
   @Element() private host!: Element;
 
-  // It is recommended to fetch the result context using the connectedCallback lifecycle method.
+  // It is recommended to fetch the result context using the connectedCallback lifecycle method
+  // with async/await. Using componentWillLoad will hang the parent atomic-search-interface initialization.
   public async connectedCallback() {
     try {
       this.result = await resultContext(this.host);
@@ -25,6 +31,7 @@ export class SampleResultComponent {
   }
 
   public render() {
+    // Do not render the component until the result object as been resolved.
     if (!this.result) {
       return;
     }
