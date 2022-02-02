7434cf5c57474433ce586b20cce470fd130d63e8
Louis Bompart <lbompart@coveo.com>
2022-02-02 18:32:03 -0500

    screw this

--- scripts/snyk_prepare.sh
+++ scripts/snyk_prepare.sh
@@ -1,6 +1,1 @@
-rsync -r -m --include=\"*/\" --include=\"package.json\" --exclude=\"*\" node_modules/ node_modules_bak
-npm i --no-package-lock
-rsync -r -m --existing --include=\"*/\" --include=\"package.json\" --exclude=\"*\" ../../node_modules .
-rsync -r -m --existing --include=\"*/\" --include=\"package.json\" --exclude=\"*\" node_modules_bak/ node_modules
-npm i --package-lock-only
-rm -rf node_modules_bak
+npm i
