e0df588e51b50b97a2d691c1741f80c9fd6d0d25
Louis Bompart <lbompart@coveo.com>
2022-02-02 16:19:45 -0500

    methack
    https://coveord.atlassian.net/browse/CDX-794

---
+++ scripts/snyk_prepare.sh
@@ -0,0 +1,5 @@
+rsync -r -m --include=\"*/\" --include=\"package.json\" --exclude=\"*\" node_modules/ node_modules_bak
+npm i --no-package-lock
+rsync -r -m --existing --include=\"*/\" --include=\"package.json\" --exclude=\"*\" ../../node_modules .
+rsync -r -m --existing --include=\"*/\" --include=\"package.json\" --exclude=\"*\" node_modules_bak/ node_modules
+npm i --package-lock-only
