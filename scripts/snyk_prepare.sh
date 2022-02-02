272237a9a7c44644213c8d2149c8ccb943372257
Louis Bompart <lbompart@coveo.com>
2022-02-02 16:40:05 -0500

    remove bak
    https://coveord.atlassian.net/browse/CDX-794

--- scripts/snyk_prepare.sh
+++ scripts/snyk_prepare.sh
@@ -3,3 +3,4 @@ npm i --no-package-lock
 rsync -r -m --existing --include=\"*/\" --include=\"package.json\" --exclude=\"*\" ../../node_modules .
 rsync -r -m --existing --include=\"*/\" --include=\"package.json\" --exclude=\"*\" node_modules_bak/ node_modules
 npm i --package-lock-only
+rm -rf node_modules_bak
