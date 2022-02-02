56df05f0d062bbe3950ac8dce005ca5a8a8157c4
Louis Bompart <lbompart@coveo.com>
2022-02-01 23:25:42 -0500

    npm setup->npm i

--- packages/cli-e2e/entrypoints/dockerHeadless.sh
+++ packages/cli-e2e/entrypoints/dockerHeadless.sh
@@ -10,7 +10,7 @@ npm install -g @angular/cli
 rsync -r --exclude="node_modules" /home/notGroot/cli/* /home/notGroot/cli-copy/
 cd /home/notGroot/cli-copy
 
-npm run setup
+npm i
 npm run build
 export UI_TEMPLATE_VERSION=0.0.0
 npm set registry http://verdaccio:4873
