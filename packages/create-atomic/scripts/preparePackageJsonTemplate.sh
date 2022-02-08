c6aacddc522ee82a7372451874dc2e49f5872295
Louis Bompart <lbompart@coveo.com>
2022-02-08 18:27:52 -0500

    replace jq by js script
    https://coveord.atlassian.net/browse/CDX-794

--- packages/create-atomic/scripts/preparePackageJsonTemplate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Copy ./templates/package.json to ./templates/package.json.hbs, and:
-#   * Replace name by "{{project}}"
-#   * Add "postinstall": "npm run setup-lambda && npm run setup-cleanup" to the scripts
-jq '.name="{{project}}" | .scripts.postinstall= "npm run setup-lambda && npm run setup-cleanup"' ./templates/package.json > ./templates/package.json.hbs
