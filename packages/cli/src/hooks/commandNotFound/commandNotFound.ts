d67aad7e3add7fbf7ba0389372d4efd9664e83a6
y-lakhdar <ylakhdar@coveo.com>
2022-02-09 20:02:40 -0500

    fix command_not_found
    https://coveord.atlassian.net/browse/CDX-746

--- packages/cli/src/hooks/commandNotFound/commandNotFound.ts
+++ packages/cli/src/hooks/commandNotFound/commandNotFound.ts
@@ -1,4 +1,5 @@
 import {Hook} from '@oclif/core';
+import {CLIError} from '@oclif/core/lib/errors';
 import {buildEvent} from '../analytics/eventUtils';
 
 const hook: Hook<'command_not_found'> = async function (options) {
@@ -8,6 +9,8 @@ const hook: Hook<'command_not_found'> = async function (options) {
       error_type: 'COMMAND NOT FOUND',
     }),
   });
+
+  throw new CLIError(`command ${options.id} not found`);
 };
 
 export default hook;
