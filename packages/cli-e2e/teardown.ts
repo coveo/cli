ab1c6a19966815c070ab97e3bd534104e6803344
Louis Bompart <lbompart@coveo.com>
2022-02-10 19:09:35 -0500

    do not attempt to teardown browser while not in CI

--- packages/cli-e2e/teardown.ts
+++ packages/cli-e2e/teardown.ts
@@ -1,10 +1,13 @@
 import {closeAllPages, connectToChromeBrowser} from './utils/browser';
 
 export default async function () {
-  const browser = await connectToChromeBrowser();
-  const pageClosePromises = await closeAllPages(browser);
-  if (global.processManager) {
-    await global.processManager.killAllProcesses();
+  const cleaningPromises = [Promise.resolve()];
+  if (process.env.CI) {
+    const browser = await connectToChromeBrowser();
+    cleaningPromises.push(...(await closeAllPages(browser)));
   }
-  return Promise.all(pageClosePromises);
+  if (global.processManager) {
+    cleaningPromises.push(global.processManager.killAllProcesses());
+  }
+  await Promise.all(cleaningPromises);
 }
