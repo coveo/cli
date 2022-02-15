db778d99318ab1cfdec9073eb12aa1de215b9c27
y-lakhdar <ylakhdar@coveo.com>
2022-02-15 10:38:52 -0500

    add mocks

--- packages/cli/src/hooks/analytics/identifier.spec.ts
+++ packages/cli/src/hooks/analytics/identifier.spec.ts
@@ -7,7 +7,21 @@ jest.mock('../../lib/config/globalConfig');
 jest.mock('@oclif/core');
 jest.mock('os');
 
+import {release} from 'os';
+import {Identify} from '@amplitude/identify';
+import {Config} from '../../lib/config/config';
+import {AuthenticatedClient} from '../../lib/platform/authenticatedClient';
+import PlatformClient from '@coveord/platform-client';
+import globalConfig from '../../lib/config/globalConfig';
+
 describe('identifier', () => {
+  jest.mocked(globalConfig);
+  jest.mocked(Config);
+  jest.mocked(Identify, true);
+  jest.mocked(AuthenticatedClient);
+  jest.mocked(PlatformClient);
+  jest.mocked(release);
+
   it('bla bla', () => {
     expect(1).toBe(1);
   });
