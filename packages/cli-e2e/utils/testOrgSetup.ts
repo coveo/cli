044ed1326f9e7072cffd14822761a91da1b43f2d
Louis Bompart <lbompart@coveo.com>
2022-03-01 14:55:49 -0500

    fix test orgid
    https://coveord.atlassian.net/browse/CDX-860

--- packages/cli-e2e/utils/testOrgSetup.ts
+++ packages/cli-e2e/utils/testOrgSetup.ts
@@ -1,20 +1,11 @@
-import {getConfig as getCliConfig, getEnvFilePath} from './cli';
-import {saveToEnvFile} from './file';
-import {createOrg, getPlatformHost} from './platform';
+import {getConfig as getCliConfig} from './cli';
+import {createOrg} from './platform';
 
 export async function getTestOrg() {
   const testOrgName = `cli-e2e-${process.env.TEST_RUN_ID}`;
   const {accessToken} = getCliConfig();
   const testOrgId = await createOrg(testOrgName, accessToken);
   console.log(`Created org ${testOrgId}`);
-  const pathToEnv = getEnvFilePath();
-  saveToEnvFile(pathToEnv, {
-    PLATFORM_ENV: process.env.PLATFORM_ENV,
-    PLATFORM_HOST: getPlatformHost(process.env.PLATFORM_ENV || ''),
-    TEST_RUN_ID: process.env.TEST_RUN_ID,
-    TEST_ORG_ID: testOrgName,
-    ACCESS_TOKEN: accessToken,
-  });
 
-  return testOrgName;
+  return testOrgId;
 }
