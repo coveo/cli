fed26b35b5685d0c31a75387cce40dcd03dc4e35
Louis Bompart <lbompart@coveo.com>
2022-03-03 01:25:01 -0500

    chore(clean): add missing env (#701)
    * Update delete-resources.yml
    
    * hackish, but it should work :tm:
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * facepalm
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * browser setup
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * psmg&kill
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * typo
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * fix matcher
    
    https://coveord.atlassian.net/browse/CDX-862
    
    fix other matcher
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * no chrome?
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * now
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * Revert "no chrome?"
    
    This reverts commit 6046be634b7ad9c4c4036735f84e39d1ba0c1968.
    https://coveord.atlassian.net/browse/CDX-862
    
    * cleaning
    
    https://coveord.atlassian.net/browse/CDX-862
    
    * cleaning
    
    https://coveord.atlassian.net/browse/CDX-862

---
+++ packages/cli-e2e/cleaning.ts
@@ -0,0 +1,20 @@
+import {mkdirSync} from 'fs';
+import {launch as launchChrome} from 'chrome-launcher';
+import {connectToChromeBrowser, SCREENSHOTS_PATH} from './utils/browser';
+import {loginWithOffice} from './utils/login';
+import {getPlatformHost} from './utils/platform';
+import waitOn from 'wait-on';
+import 'dotenv/config';
+import {ProcessManager} from './utils/processManager';
+(async () => {
+  mkdirSync(SCREENSHOTS_PATH, {recursive: true});
+  global.processManager = new ProcessManager();
+  process.env.PLATFORM_ENV = process.env.PLATFORM_ENV?.toLowerCase() || '';
+  process.env.PLATFORM_HOST = getPlatformHost(process.env.PLATFORM_ENV);
+  const chrome = await launchChrome({port: 9222, userDataDir: false});
+  await waitOn({resources: ['tcp:9222']});
+  const browser = await connectToChromeBrowser();
+  await loginWithOffice(browser);
+  await chrome.kill();
+  await global.processManager.killAllProcesses();
+})();
