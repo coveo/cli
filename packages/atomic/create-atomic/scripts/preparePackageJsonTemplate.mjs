2ea6ce58c6ed5ab6777868948089d44699c8579c
Louis Bompart <lbompart@coveo.com>
2022-03-08 15:42:25 -0500

    chore(atomic): split off create-atomic and its template
    https://coveord.atlassian.net/browse/CDX-868

--- packages/atomic/create-atomic/scripts/preparePackageJsonTemplate.mjs
+++ packages/atomic/create-atomic/scripts/preparePackageJsonTemplate.mjs
@@ -4,9 +4,16 @@ import detectIndent from 'detect-indent';
 import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// https://nodejs.org/docs/latest-v16.x/api/esm.html#importmetaresolvespecifier-parent
+const atomicTemplatePath = await import.meta.resolve(
+  '@coveo/create-atomic-template'
+);
+const bundledTemplatePath = resolve(__dirname, '..', 'template');
+
+copySync(atomicTemplatePath, bundledTemplatePath, {recursive: true});
 
 const packageJson = readFileSync(
-  resolve(__dirname, '..', 'template', 'package.json'),
+  join(bundledTemplatePath, 'package.json'),
   'utf-8'
 );
 
