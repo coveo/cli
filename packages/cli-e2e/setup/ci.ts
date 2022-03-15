975fc0ca7689da632649ebfe944ee275fa865cb8
Louis Bompart <lbompart@coveo.com>
2022-03-15 18:45:49 -0400

    showme the cache
    https://coveord.atlassian.net/browse/CDX-868

--- packages/cli-e2e/setup/ci.ts
+++ packages/cli-e2e/setup/ci.ts
@@ -4,7 +4,9 @@ import {getConfigFilePath} from '../utils/cli';
 import {ProcessManager} from '../utils/processManager';
 
 import {setProcessEnv, createUiProjectDirectory, startVerdaccio} from './utils';
-import {dirname} from 'path';
+import {dirname, join, resolve} from 'path';
+import {npm} from '../utils/npm';
+import {Terminal} from '../utils/terminal/terminal';
 
 export default async function () {
   mkdirSync(SCREENSHOTS_PATH, {recursive: true});
@@ -14,6 +16,18 @@ export default async function () {
   createUiProjectDirectory();
   global.processManager = new ProcessManager();
   await startVerdaccio();
+  const args = [...npm(), 'cache', 'ls', '@coveo/create-atomic'];
+  await new Terminal(
+    args.shift()!,
+    args,
+    {cwd: resolve(join(__dirname, '..'))},
+    global.processManager!,
+    'npm-cache'
+  )
+    .when('exit')
+    .on('process')
+    .do()
+    .once();
   restoreCliConfig();
 }
 
