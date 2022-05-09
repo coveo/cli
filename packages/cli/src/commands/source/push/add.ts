c2e472ce4992ee9b81e1a609b13abbbb225bbd1e
Yassine <ylakhdar@coveo.com>
2022-05-09 10:47:47 -0400

    feat: introduce the files flag (#783)
    https://coveord.atlassian.net/browse/CDX-952

--- packages/cli/src/commands/source/push/add.ts
+++ packages/cli/src/commands/source/push/add.ts
@@ -1,8 +1,6 @@
 import {PushSource, UploadBatchCallbackData} from '@coveo/push-api-client';
 import {Command, Flags, CliUx} from '@oclif/core';
 import {green} from 'chalk';
-import {readdirSync} from 'fs';
-import {join} from 'path';
 import {
   HasNecessaryCoveoPrivileges,
   IsAuthenticated,
@@ -22,20 +20,25 @@ export default class SourcePushAdd extends Command {
     'Push a JSON document into a Coveo Push source. See https://github.com/coveo/cli/wiki/Pushing-JSON-files-with-Coveo-CLI for more information.';
 
   public static flags = {
+    // TODO: CDX-856: remove file flag
     file: Flags.string({
+      // For retro compatibility
       multiple: true,
-      exclusive: ['folder'],
+      hidden: true,
+    }),
+    // TODO: CDX-856: remove folder flag
+    folder: Flags.string({
+      // For retro compatibility
+      multiple: true,
+      char: 'd',
+      hidden: true,
+    }),
+    files: Flags.string({
+      multiple: true,
       char: 'f',
       helpValue: 'myfile.json',
-      description: 'One or multiple file to push. Can be repeated.',
-    }),
-    folder: Flags.string({
-      multiple: true,
-      exclusive: ['file'],
-      char: 'd',
-      helpValue: './my_folder_with_multiple_json_files',
       description:
-        'One or multiple folder containing json files. Can be repeated',
+        'Combinaison of JSON files and folders (containing JSON files) to push. Can be repeated.',
     }),
     maxConcurrent: Flags.integer({
       exclusive: ['file'],
@@ -72,12 +75,8 @@ export default class SourcePushAdd extends Command {
     )
   )
   public async run() {
+    await this.showDeprecatedFlagWarning();
     const {args, flags} = await this.parse(SourcePushAdd);
-    if (!flags.file && !flags.folder) {
-      this.error(
-        'You must minimally set the `file` or the `folder` flag. Use `source:push:add --help` to get more information.'
-      );
-    }
     const {accessToken, organization, environment, region} =
       await new AuthenticatedClient().cfg.get();
     const source = new PushSource(accessToken!, organization, {
@@ -85,7 +84,7 @@ export default class SourcePushAdd extends Command {
       region,
     });
 
-    CliUx.ux.action.start('Processing...');
+    CliUx.ux.action.start('Processing files');
 
     const fileNames = await this.getFileNames();
     const options = {
@@ -102,7 +101,7 @@ export default class SourcePushAdd extends Command {
 
     await source.setSourceStatus(args.sourceId, 'IDLE');
 
-    CliUx.ux.action.stop();
+    CliUx.ux.action.stop(green('✔'));
   }
 
   @Trackable()
@@ -112,20 +111,16 @@ export default class SourcePushAdd extends Command {
 
   private async getFileNames() {
     const {flags} = await this.parse(SourcePushAdd);
-    let fileNames: string[] = [];
-    if (flags.file) {
-      fileNames = fileNames.concat(flags.file);
-    }
-    if (flags.folder) {
-      const isString = (file: string | null): file is string => Boolean(file);
-      fileNames = fileNames.concat(
-        flags.folder
-          .flatMap((folder) =>
-            readdirSync(folder, {withFileTypes: true}).map((dirent) =>
-              dirent.isFile() ? join(folder, dirent.name) : null
-            )
-          )
-          .filter(isString)
+    const fileNames = [
+      // TODO: CDX-856: only read --files flag
+      ...(flags.file ?? []),
+      ...(flags.folder ?? []),
+      ...(flags.files ?? []),
+    ];
+
+    if (fileNames.length === 0) {
+      this.error(
+        'You must set the `files` flag. Use `source:push:add --help` to get more information.'
       );
     }
     return fileNames;
@@ -148,6 +143,14 @@ export default class SourcePushAdd extends Command {
       } accepted by the Push API from ${green(fileNames)}.`,
       res
     );
+  }
+
+  private async showDeprecatedFlagWarning() {
+    // TODO: CDX-856: no longer needed once flags are removed
+    const {flags} = await this.parse(SourcePushAdd);
+    if (flags.file || flags.folder) {
+      CliUx.ux.warn('Use the `files` flag instead');
+    }
   }
 
   private errorMessageOnAdd(err: unknown) {
