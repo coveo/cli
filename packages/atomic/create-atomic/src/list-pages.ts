b7424cbb5b284bccba47295f6767c31bd0c06964
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2022-05-26 14:01:13 -0400

    add per page param
    https://coveord.atlassian.net/browse/CDX-771

--- packages/atomic/create-atomic/src/list-pages.ts
+++ packages/atomic/create-atomic/src/list-pages.ts
@@ -1,8 +1,10 @@
 import {PlatformClient} from '@coveord/platform-client';
 
 export async function listSearchPagesOptions(client: PlatformClient) {
-  return (await client.searchInterfaces.list({})).items.map((page) => ({
-    value: page.id,
-    name: page.name,
-  }));
+  return (await client.searchInterfaces.list({perPage: 150})).items.map(
+    (page) => ({
+      value: page.id,
+      name: page.name,
+    })
+  );
 }
