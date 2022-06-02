e3391bb1d20546a0a20658c307b0601daf839d51
y-lakhdar <ylakhdar@coveo.com>
2022-06-02 11:13:20 -0400

    remove comment
    https://coveord.atlassian.net/browse/CDX-978

--- packages/cli/src/commands/source/catalog/add.ts
+++ packages/cli/src/commands/source/catalog/add.ts
@@ -75,10 +75,6 @@ export default class SourceCatalogAdd extends Command {
   public async run() {
     const {args, flags} = await this.parse(SourceCatalogAdd);
 
-    /* TODO: ask user if want to create catalog.
-       This can be a great time saver, especially if the user has nothing in his organization yet.
-    */
-
     if (
       !flags.fullUpload &&
       !flags.skipFullUploadCheck &&
