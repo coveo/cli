00c5cd176008beb5fad17275d973c008248957b9
Yassine <ylakhdar@coveo.com>
2022-06-02 10:25:41 -0400

    feat: extract fields and objecttype values (#789)
    https://coveord.atlassian.net/browse/CDX-977

--- packages/cli/src/commands/commerce/catalog/create.ts
+++ packages/cli/src/commands/commerce/catalog/create.ts
@@ -1,12 +1,13 @@
 import {bold} from 'chalk';
 import type {
+  FieldModel,
   CreateCatalogConfigurationModel,
   CatalogConfigurationModel,
   CatalogFieldsMapping,
   New,
   PlatformClient,
 } from '@coveord/platform-client';
-import {Command, Flags} from '@oclif/core';
+import {CliUx, Command, Flags} from '@oclif/core';
 import {
   HasNecessaryCoveoPrivileges,
   IsAuthenticated,
@@ -14,6 +15,15 @@ import {
 } from '../../../lib/decorators/preconditions';
 import {Trackable} from '../../../lib/decorators/preconditions/trackable';
 import {AuthenticatedClient} from '../../../lib/platform/authenticatedClient';
+import {
+  BuiltInTransformers,
+  DocumentBuilder,
+  FieldAnalyser,
+  MetadataValue,
+  parseAndGetDocumentBuilderFromJSONDocument,
+} from '@coveo/push-api-client';
+import {PathLike} from 'fs';
+import dedent from 'ts-dedent';
 
 export default class CatalogCreate extends Command {
   public static description = `${bold.bgYellow(
@@ -21,31 +31,17 @@ export default class CatalogCreate extends Command {
   )} Create a commerce catalog interactively`;
 
   public static flags = {
-    // TODO: find an alternative to skip all the prompts without having too many flags.
-    withVariants: Flags.boolean({
-      char: 'v',
-      default: false,
-      helpGroup: 'Catalog structure',
-      description: 'Whether your catalog contains product variants',
-    }),
-    withAvailabilites: Flags.boolean({
-      char: 'a',
-      default: false,
-      helpGroup: 'Catalog structure',
-      description:
-        'Whether your catalog contains product location and/or availabilities',
-    }),
     dataFiles: Flags.string({
       multiple: true,
       char: 'f',
+      required: true,
       helpValue: 'products.json',
       description:
         'Combinaison of JSON files and folders (containing JSON files) to push. Can be repeated.',
     }),
     availabilityFiles: Flags.string({
       multiple: true,
-      char: 'A',
-      dependsOn: ['withavailabilites'],
+      char: 'a',
       helpValue: 'availabilities.json',
       description:
         'Combinaison of JSON files and folders (containing JSON files) to push. Can be repeated.',
@@ -70,18 +66,14 @@ export default class CatalogCreate extends Command {
     // TODO: Add edit catalog privilege. https://docs.coveo.com/en/2956/coveo-for-commerce/index-commerce-catalog-content-with-the-stream-api#required-privileges
   )
   public async run() {
-    // TODO: CDX-977: parse documents to extract metadata keys
-    // TODO: CDX-977: throw error if no objecttype is found. throw warning if some documents are missing the objecttype
-    // TODO: CDX-977: (optional) throw warning for duplicate SKUs
+    this.printWarningMessage();
+    const client = await new AuthenticatedClient().getClient();
+    const {fields, objectTypes} = await this.parseDocuments(client);
 
-    // TODO: CDX-978: prompt user what objecttype value should define products (and possibly variants and availabilities).
-    // TODO: CDX-978: prompt user what fields should be used as catalog ids. This steps should leverage the metadata keys found in the previous step
-    // TODO: CDX-978: prompt user for standard field mappings. Only if the CLI is not able to resolve them in the first place
-    // TODO: CDX-978: prompt user for source ids. If the there are no sources in the org, create them
+    await this.promptQuestions(fields, objectTypes);
     await this.ensureCatalogValidity();
 
     // Destructive changes starting from here
-    const client = await new AuthenticatedClient().getClient();
     const configuration = await this.createCatalogConfiguration(client);
     await this.createCatalog(client, configuration);
   }
@@ -91,9 +83,66 @@ export default class CatalogCreate extends Command {
     throw err;
   }
 
+  private async parseDocuments(client: PlatformClient) {
+    const {flags} = await this.parse(CatalogCreate);
+    const analyser = new FieldAnalyser(client);
+    const docBuilders: DocumentBuilder[] = [];
+    const objectTypeValues: Set<MetadataValue> = new Set();
+    const callback = async (docBuilder: DocumentBuilder, docPath: PathLike) => {
+      const {metadata, uri} = docBuilder.build();
+      if (metadata?.objecttype === undefined) {
+        CliUx.ux.warn(
+          `missing ${bold('objecttype')} metadata on item ${uri} (${docPath})`
+        );
+      } else {
+        objectTypeValues.add(metadata.objecttype);
+      }
+    };
+
+    for (const filePath of flags.dataFiles.values()) {
+      docBuilders.push(
+        ...parseAndGetDocumentBuilderFromJSONDocument(filePath, {
+          callback,
+          fieldNameTransformer: BuiltInTransformers.toLowerCase,
+        })
+      );
+    }
+
+    if (objectTypeValues.size === 0) {
+      CliUx.ux.error(
+        dedent`
+        No ${bold('objecttype')} metadata detected while parsing documents.
+        The ${bold(
+          'objecttype'
+        )} metadata is crucial, as it will be used to identify the item as a product in the index. Ensure this metadata is set on all your items.
+        See https://docs.coveo.com/en/m53g7119 for more information.`
+      );
+    }
+
+    await analyser.add(docBuilders);
+    return {
+      objectTypes: Array.from(objectTypeValues.values()),
+      fields: analyser.report().fields,
+    };
+  }
+
+  private async promptQuestions(
+    _fields: FieldModel[],
+    _objectTypes: MetadataValue[]
+  ) {
+    // TODO: CDX-978: prompt user what objecttype value should define products (and possibly variants and availabilities).
+    // TODO: CDX-978: prompt user what fields should be used as catalog ids. This steps should leverage the metadata keys found in the previous step
+    // TODO: CDX-978: prompt user for standard field mappings. Only if the CLI is not able to resolve them in the first place
+    // TODO: CDX-978: prompt user for source ids. If the there are no sources in the org, create them
+  }
+
+  /**
+   * (Optional for phase 0)
+   * Parse data a second time to see if the provided inputs from the user can generate a valid catalog
+   * This would prevent going forward with a broken/invalid catalog.
+   */
   private async ensureCatalogValidity() {
-    // TODO: (Optional for phase 0) parse documents a second time to see if the provided inputs from the user can generate a valid catalog
-    // This would prevent going forward with a broken/invalid catalog.
+    // TODO: Throw warning whenever duplicate SKUs are detected
     return;
   }
 
@@ -123,6 +172,12 @@ export default class CatalogCreate extends Command {
       catalogConfigurationId: catalogConfiguration.id,
       name: args.name,
     });
+  }
+
+  private printWarningMessage() {
+    CliUx.ux.warn(
+      'The `commerce:catalog:create` command is currently in alpha, use at your own risk'
+    );
   }
 
   private getFieldMappings(): CatalogFieldsMapping {
