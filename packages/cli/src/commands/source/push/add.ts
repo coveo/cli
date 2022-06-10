dbc3a3f2f2fcb558442654537c9ce890936e11ff
y-lakhdar <ylakhdar@coveo.com>
2022-06-10 11:55:40 -0400

    bring back finally

--- packages/cli/src/commands/source/push/add.ts
+++ packages/cli/src/commands/source/push/add.ts
@@ -101,6 +101,12 @@ export default class SourcePushAdd extends Command {
     throw err;
   }
 
+  public async finally() {
+    const {args} = await this.parse(SourcePushAdd);
+    const source = await this.getSource();
+    await source.setSourceStatus(args.sourceId, 'IDLE');
+  }
+
   public async getSource() {
     const {accessToken, organization, environment, region} =
       await new AuthenticatedClient().cfg.get();
