135b43c9c6d6646c914285f5f978f21287513303
ylakhdar <ylakhdar@coveo.com>
2022-07-06 15:20:18 -0400

    copy folder parse from push-api-client
    https://coveord.atlassian.net/browse/CDX-1065

--- packages/cli/src/lib/utils/file.ts
+++ packages/cli/src/lib/utils/file.ts
@@ -1,4 +1,6 @@
 import {CliUx} from '@oclif/core';
+import {lstatSync, PathLike, readdirSync} from 'fs';
+import {join} from 'path';
 
 export interface FilesOrFolders {
   file?: string[]; // TODO: CDX-856: remove flag
@@ -22,3 +24,41 @@ export async function getFileNames(entries: FilesOrFolders) {
 
   return entryNames;
 }
+
+// TODO: delete and export the same method in push-api-client instead
+export const isJsonFile = (documentPath: PathLike) => {
+  return documentPath.toString().endsWith('.json');
+};
+
+export const getAllJsonFilesFromEntries = (
+  filesOrDirectories: string[],
+  fileNames: string[] = []
+): string[] => {
+  const uniqueFileNames: string[] = [];
+
+  filesOrDirectories.flatMap((entry) => {
+    if (lstatSync(entry).isDirectory()) {
+      recursiveDirectoryRead(entry, fileNames);
+    } else {
+      fileNames.push(entry);
+    }
+  });
+
+  for (const file of fileNames) {
+    if (!uniqueFileNames.includes(file) && isJsonFile(file)) {
+      uniqueFileNames.push(file);
+    }
+  }
+
+  return uniqueFileNames;
+};
+
+const recursiveDirectoryRead = (folder: string, accumulator: string[] = []) => {
+  readdirSync(folder, {withFileTypes: true}).map((dirent) => {
+    if (dirent.isDirectory()) {
+      recursiveDirectoryRead(join(folder, dirent.name), accumulator);
+    } else if (dirent.isFile()) {
+      accumulator.push(join(folder, dirent.name));
+    }
+  });
+};
