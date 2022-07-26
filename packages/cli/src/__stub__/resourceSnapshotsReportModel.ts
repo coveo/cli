171dd924acfe23c8d751d899958ce206fe039874
ylakhdar <ylakhdar@coveo.com>
2022-07-26 10:15:15 -0400

    adjust snapshotReporter
    https://coveord.atlassian.net/browse/CDX-1090

--- packages/cli/src/__stub__/resourceSnapshotsReportModel.ts
+++ packages/cli/src/__stub__/resourceSnapshotsReportModel.ts
@@ -123,49 +123,13 @@ export const getSuccessReport = (
 export const getErrorReport = (
   snapshotId: string,
   type: ResourceSnapshotsReportType
-): ResourceSnapshotsReportModel => ({
-  id: snapshotId,
-  type: type,
-  updatedDate: 1622555847000,
-  resourcesProcessed: 11,
-  status: ResourceSnapshotsReportStatus.Completed,
-  resultCode: ResourceSnapshotsReportResultCode.ResourcesInError,
-  resourceOperations: {
-    EXTENSION: {
-      resourcesCreated: 1,
-      resourcesUpdated: 0,
-      resourcesRecreated: 0,
-      resourcesDeleted: 2,
-      resourcesInError: 0,
-      resourcesUnchanged: 0,
-    },
-    FIELD: {
-      resourcesCreated: 0,
-      resourcesUpdated: 1,
-      resourcesRecreated: 0,
-      resourcesDeleted: 0,
-      resourcesInError: 7,
-      resourcesUnchanged: 0,
-    },
-  },
-  resourceOperationResults: {
-    FIELD: {
-      foo_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field foo already exists.'],
-      bar_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field bar already exists.'],
-      dsads_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field dsads already exists.'],
-      fdww_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field fdww already exists.'],
-      csad_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field csad already exists.'],
-      hjkd_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field hjkd already exists.'],
-      fdasf_4VNj5ds5: ['RESOURCE_ALREADY_EXISTS: Field fdasf already exists.'],
-    },
-  },
-});
+): ResourceSnapshotsReportModel => {
+  const buildOperationResultError = (field: string) => ({
+    resultCode: 'RESOURCE_ALREADY_EXISTS',
+    message: `Field ${field} already exists.`,
+  });
 
-export const getMissingVaultEntryReport = (
-  snapshotId: string,
-  type: ResourceSnapshotsReportType
-): ResourceSnapshotsReportModel =>
-  ({
+  return {
     id: snapshotId,
     type: type,
     updatedDate: 1622555847000,
@@ -192,32 +156,61 @@ export const getMissingVaultEntryReport = (
     },
     resourceOperationResults: {
       FIELD: {
-        foo_4VNj5ds5: [
-          {
-            resultCode: 'INVALID_PLACEHOLDER',
-            message:
-              'The vault entry referenced by {{ VAULT.foo_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-          },
-        ],
-        bar_4VNj5ds5: [
-          'The vault entry referenced by {{ VAULT.bar_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-        ],
-        dsads_4VNj5ds5: [
-          'The vault entry referenced by {{ VAULT.dsads_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-        ],
-        fdww_4VNj5ds5: [
-          'The vault entry referenced by {{ VAULT.fdww_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-        ],
-        csad_4VNj5ds5: [
-          'The vault entry referenced by {{ VAULT.csad_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-        ],
-        hjkd_4VNj5ds5: [
-          'The vault entry referenced by {{ VAULT.hjkd_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-        ],
-        fdasf_4VNj5ds5: [
-          'The vault entry referenced by {{ VAULT.fdasf_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.',
-        ],
+        foo_4VNj5ds5: [buildOperationResultError('foo')],
+        bar_4VNj5ds5: [buildOperationResultError('bar')],
+        dsads_4VNj5ds5: [buildOperationResultError('dsads')],
+        fdww_4VNj5ds5: [buildOperationResultError('fdww')],
+        csad_4VNj5ds5: [buildOperationResultError('csad')],
+        hjkd_4VNj5ds5: [buildOperationResultError('hjkd')],
+        fdasf_4VNj5ds5: [buildOperationResultError('fdasf')],
       },
     },
-    // TODO CDX-1038
-  } as unknown as ResourceSnapshotsReportModel);
+  };
+};
+
+export const getMissingVaultEntryReport = (
+  snapshotId: string,
+  type: ResourceSnapshotsReportType
+): ResourceSnapshotsReportModel => {
+  const buildInvalidPlaceholderReportOperationResult = (field: string) => ({
+    resultCode: 'INVALID_PLACEHOLDER',
+    message: `The vault entry referenced by {{ VAULT.${field}_4VNj5ds5-configuration.userIdentities.UserIdentity.password }} could not be found in the vault.`,
+  });
+  return {
+    id: snapshotId,
+    type: type,
+    updatedDate: 1622555847000,
+    resourcesProcessed: 11,
+    status: ResourceSnapshotsReportStatus.Completed,
+    resultCode: ResourceSnapshotsReportResultCode.ResourcesInError,
+    resourceOperations: {
+      EXTENSION: {
+        resourcesCreated: 1,
+        resourcesUpdated: 0,
+        resourcesRecreated: 0,
+        resourcesDeleted: 2,
+        resourcesInError: 0,
+        resourcesUnchanged: 0,
+      },
+      FIELD: {
+        resourcesCreated: 0,
+        resourcesUpdated: 1,
+        resourcesRecreated: 0,
+        resourcesDeleted: 0,
+        resourcesInError: 7,
+        resourcesUnchanged: 0,
+      },
+    },
+    resourceOperationResults: {
+      FIELD: {
+        foo_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('foo')],
+        bar_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('bar')],
+        dsads_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('dsads')],
+        fdww_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('fdww')],
+        csad_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('csad')],
+        hjkd_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('hjkd')],
+        fdasf_4VNj5ds5: [buildInvalidPlaceholderReportOperationResult('fdasf')],
+      },
+    },
+  };
+};
