d9b6a27aa38aa234054c09bd27b066e23989d834
Louis Bompart <lbompart@coveo.com>
2022-08-22 14:08:25 -0400

    refactor

--- packages/cli/commons/src/config/stub.ts
+++ packages/cli/commons/src/config/stub.ts
@@ -1,6 +1,6 @@
 import {Region} from '@coveord/platform-client';
-import type {Config, Configuration} from '@coveo/cli-commons/lib/config/config';
-import {PlatformEnvironment} from '@coveo/cli-commons/lib/platform/environment';
+import type {Config, Configuration} from './config';
+import {PlatformEnvironment} from '../platform/environment';
 
 export const defaultConfiguration = {
   environment: PlatformEnvironment.Dev,
