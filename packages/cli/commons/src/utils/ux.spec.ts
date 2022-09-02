71cb2e2bb110f12be4a32ac1c415ddd2e625a24a
ylakhdar <ylakhdar@coveo.com>
2022-09-02 13:37:13 -0400

    unit test
    https://coveord.atlassian.net/browse/CDX-1114

---
+++ packages/cli/commons/src/utils/ux.spec.ts
@@ -0,0 +1,56 @@
+import {fancyIt} from '@coveo/cli-commons-dev/testUtils/it';
+import {stderr} from 'stdout-stderr';
+import {startSpinner, stopSpinner} from './ux';
+
+describe('ux', () => {
+  describe('when spinner is ended with no argument', () => {
+    beforeEach(() => {
+      stderr.start();
+      startSpinner('starting a process');
+    });
+
+    fancyIt()('should stop running task without error', async (ctx) => {
+      stopSpinner();
+      stderr.stop();
+      expect(ctx.stderr).toContain('starting a process... ✔');
+    });
+
+    fancyIt()(
+      'should print new task after ending previous task',
+      async (ctx) => {
+        startSpinner('installing stuff');
+        stopSpinner();
+        stderr.stop();
+        expect(ctx.stderr).toContain('starting a process... ✔');
+        expect(ctx.stderr).toContain('installing stuff... ✔');
+      }
+    );
+  });
+
+  describe.each([new Error('AARRRGgggh 😱'), 'AARRRGgggh 😱'])(
+    'when spinner is ended with an argument',
+    (message) => {
+      beforeEach(() => {
+        stderr.start();
+        startSpinner('starting a process');
+      });
+
+      fancyIt()('should stop running task with the error', async (ctx) => {
+        stopSpinner(message);
+        stderr.stop();
+        expect(ctx.stderr).toContain('starting a process... ! AARRRGgggh 😱');
+      });
+    }
+  );
+
+  describe('when no spinner is running', () => {
+    fancyIt()('should not print anything', async (ctx) => {
+      stderr.start();
+      stopSpinner();
+      stopSpinner();
+      stopSpinner();
+      stderr.stop();
+      expect(ctx.stderr).toBe('');
+    });
+  });
+});
