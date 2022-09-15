815f76f91be60ad7593a8442767a9a6bb094f08d
ylakhdar <ylakhdar@coveo.com>
2022-09-15 16:44:10 -0400

    add subset util
    https://coveord.atlassian.net/browse/CDX-647

--- packages/cli/core/src/lib/utils/list.ts
+++ packages/cli/core/src/lib/utils/list.ts
@@ -1,3 +1,7 @@
 export function without<T>(array: T[], values: T[]): T[] {
   return array.filter((field) => !values.includes(field));
 }
+
+export function isSubset<T>(subset: T[], superset: T[]): boolean {
+  return without(subset, superset).length === 0;
+}
