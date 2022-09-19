c31fdb02b68a6db3bb183b2ef04b4075c597fc45
Louis Bompart <lbompart@coveo.com>
2022-09-19 19:08:32 -0400

    chore: migrate to independent release (#929)
    <!-- For Coveo Employees only. Fill this section.
    
    CDX-1146
    
    -->
    
    ## Proposed changes
    
    Modify the release process to be able to do independent releases.
    This also means that package version will not be 'synchronized' anymore:
    So in the future, the default version of `create-atomic` for the `cli`
    might not be the version of the `cli`.
    
    ## Breaking changes
    
    ### How we do merge for changes that impact multiple projects:
    - For now: If your changes impact multiple packages, you have to
    organize your commits so that one commit affects a single workspace
    every time.
    - Soon ™️ : The PR description will be changed to include a new section
    that will be used by semantic release parser to make 'one commit nth
    commits'. This will be done in another PR
    
    For example, if I do a breaking change in `@coveo/cli-commons`, I will
    need to change how `@coveo/cli` use the latter.
    I will need to organize my branch in two commits:
    - The first one will contain only the change relative to
    `@coveo/cli-commons`. Its message will contain the breaking change
    identifier.
    - The second one will contain only the change relative to `@coveo/cli`.
    Its message will not contain the breaking change container, it will be a
    chore or feat depending on the impact on the user. (e.g. if the change
    is not impacting the end user of the CLI, then it's a chore. If the
    change adds new behavior for the end user, it's a feat)
    
    For this reason, I enabled the rebase merge option on the repository. It
    will be the one to use for the scenario described above.
    ### Tags format
    
    - A tag for each release process, named `release-n`. `release-0` has
    been placed manually on the latest release.
    - A tag for each released package, named `@coveo/package-name@version`.
    
    ### GitHub release
    - Will occur only when the CLI is released.
    
    ## Testing
    
    - [x] Manual Tests: I created a test branch locally where I disabled git
    push and npm publish and tested the process.

---
+++ scripts/releaseV2/phase1-bump-all-packages.mjs
@@ -0,0 +1,148 @@
+import {
+  getLastTag,
+  parseCommits,
+  getCommits,
+  npmBumpVersion,
+  npmPublish,
+  getCurrentVersion,
+  getNextVersion,
+  generateChangelog,
+  writeChangelog,
+} from '@coveo/semantic-monorepo-tools';
+import {spawnSync} from 'child_process';
+import {appendFileSync, readFileSync, writeFileSync} from 'fs';
+import angularChangelogConvention from 'conventional-changelog-angular';
+import {dirname, resolve, join} from 'path';
+import {fileURLToPath} from 'url';
+import retry from 'async-retry';
+
+const rootFolder = resolve(dirname(fileURLToPath(import.meta.url)), '..', '..');
+// Run on each package, it generate the changelog, install the latest dependencies that are part of the workspace, publish the package.
+(async () => {
+  const PATH = '.';
+  const packageJson = JSON.parse(
+    readFileSync('package.json', {encoding: 'utf-8'})
+  );
+  const versionPrefix = packageJson.name;
+  const convention = await angularChangelogConvention;
+  // TODO: CDX-1147 Remove catch
+  const lastTag = await getLastTag(versionPrefix).catch(() =>
+    getLastTag('release-')
+  );
+  const commits = await getCommits(PATH, lastTag);
+  if (commits.length === 0) {
+    return;
+  }
+  const parsedCommits = parseCommits(commits, convention.parserOpts);
+  const bumpInfo = convention.recommendedBumpOpts.whatBump(parsedCommits);
+  const currentVersion = getCurrentVersion(PATH);
+  const newVersion = getNextVersion(currentVersion, bumpInfo);
+
+  await npmBumpVersion(newVersion, PATH, {
+    workspaceUpdateStrategy: 'UpdateExact',
+  });
+  await updateWorkspaceDependent(newVersion);
+  if (isPrivatePackage()) {
+    return;
+  }
+
+  const changelog = await generateChangelog(
+    parsedCommits,
+    newVersion,
+    {
+      host: 'https://github.com',
+      owner: 'coveo',
+      repository: 'cli',
+    },
+    convention.writerOpts
+  );
+  await writeChangelog(PATH, changelog);
+
+  await npmPublish();
+
+  await retry(
+    () => {
+      if (!isVersionPublished(packageJson.name, newVersion)) {
+        throw new Error('Version not available');
+      }
+    },
+    {retries: 30}
+  );
+  appendFileSync(
+    join(rootFolder, '.git-message'),
+    `${packageJson.name}@${newVersion}\n`
+  );
+})();
+
+async function updateWorkspaceDependent(version) {
+  const topology = JSON.parse(
+    readFileSync(join(rootFolder, 'topology.json'), {encoding: 'utf-8'})
+  );
+  const dependencyPackageJson = JSON.parse(
+    readFileSync('package.json', {encoding: 'utf-8'})
+  );
+  const dependencyPackageName = dependencyPackageJson.name.replace(
+    '@coveo/',
+    ''
+  );
+  const dependentPackages = [];
+  for (const [name, dependencies] of Object.entries(
+    topology.graph.dependencies
+  )) {
+    if (
+      dependencies.find(
+        (dependency) => dependency.target === dependencyPackageName
+      )
+    ) {
+      dependentPackages.push(name);
+    }
+  }
+
+  for (const dependentPackage of dependentPackages) {
+    const dependentPackageJsonPath = join(
+      rootFolder,
+      topology.graph.nodes[dependentPackage].data.root,
+      'package.json'
+    );
+    const dependentPackageJson = JSON.parse(
+      readFileSync(dependentPackageJsonPath, {encoding: 'utf-8'})
+    );
+    updateDependency(dependentPackageJson, dependencyPackageJson.name, version);
+    writeFileSync(
+      dependentPackageJsonPath,
+      JSON.stringify(dependentPackageJson)
+    );
+  }
+}
+
+function updateDependency(packageJson, dependency, version) {
+  for (const dependencyType of [
+    'dependencies',
+    'devDependencies',
+    'optionalDependencies',
+  ]) {
+    if (packageJson?.[dependencyType]?.[dependency]) {
+      packageJson[dependencyType][dependency] = version;
+    }
+  }
+}
+
+function isVersionPublished(packageName, version) {
+  return (
+    spawnSync(
+      appendCmdIfWindows`npm`,
+      ['show', `${packageName}@latest`, 'version'],
+      {encoding: 'utf-8'}
+    ).stdout.trim() === version
+  );
+}
+
+export const appendCmdIfWindows = (cmd) =>
+  `${cmd}${process.platform === 'win32' ? '.cmd' : ''}`;
+
+function isPrivatePackage() {
+  const packageJson = JSON.parse(
+    readFileSync('package.json', {encoding: 'utf-8'})
+  );
+  return packageJson.private;
+}
