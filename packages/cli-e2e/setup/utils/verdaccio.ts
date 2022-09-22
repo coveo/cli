8f1e82f5063071bd19b7a8bad0a2770d48de5c40
Louis Bompart <lbompart@coveo.com>
2022-09-22 00:24:14 -0400

    bypass verdaccio http
    https://coveord.atlassian.net/browse/CDX-1162

--- packages/cli-e2e/setup/utils/verdaccio.ts
+++ packages/cli-e2e/setup/utils/verdaccio.ts
@@ -3,10 +3,10 @@ import {join, resolve} from 'node:path';
 import {npm} from '../../utils/npm';
 import {Terminal} from '../../utils/terminal/terminal';
 import {appendCmdIfWindows} from './utils';
-import {manifest as getManifest, tarball as getTarball} from 'pacote';
+import {tarball as getTarball} from 'pacote';
 import {dirSync} from 'tmp';
 import {writeFileSync} from 'node:fs';
-import retry from 'async-retry';
+import {readJSONSync} from 'fs-extra';
 
 const npmRegistry = 'https://registry.npmjs.org/';
 const verdaccioRegistry = 'http://localhost:4873';
@@ -58,16 +58,26 @@ export async function publishPackages() {
   }
 }
 
+const verdaccioDataDir = join(
+  __dirname,
+  '..',
+  '..',
+  'verdaccio',
+  'verdaccio',
+  'storage',
+  'data'
+);
+
+function getManifest(packageToCheck: string) {
+  const manifestPath = join(verdaccioDataDir, packageToCheck, 'package.json');
+  return readJSONSync(manifestPath);
+}
+
 export async function uplinkMissingPackages() {
   const tmpdir = dirSync().name;
   for (const packageToCheck of verdaccioedPackages) {
-    const manifest = await getManifest(packageToCheck, {
-      registry: verdaccioRegistry,
-      retry: {
-        fetchRetries: 10,
-      },
-    });
-    if (manifest.version !== '0.0.0') {
+    const manifest = getManifest(packageToCheck);
+    if (manifest['dist-tags'].latest !== '0.0.0') {
       continue;
     }
     const tarballBytes = await getTarball(packageToCheck, {
