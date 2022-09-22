65f2538cb978695f2228751f2072d96dd5a09c9b
ylakhdar <ylakhdar@coveo.com>
2022-09-22 17:03:59 -0400

    remove memfs
    https://coveord.atlassian.net/browse/CDX-647

--- packages/cli/core/src/lib/utils/file.spec.ts
+++ packages/cli/core/src/lib/utils/file.spec.ts
@@ -1,27 +1,20 @@
-jest.mock('fs', () => ({
-  __esModule: true,
-  ...jest.requireActual('memfs'),
-}));
+jest.mock('fs');
 
 import {fancyIt} from '@coveo/cli-commons-dev/testUtils/it';
 import {fileDepthSearch} from './file';
-import {vol} from 'memfs';
+import {readdirSync} from 'fs';
+import {getDirectory, getFile} from '../../__test__/fsUtils';
+
+const mockedReaddirSync = jest.mocked(readdirSync);
 
 describe('File', () => {
-  beforeEach(() => {
-    vol.reset();
-  });
-
   describe('when the directory contains a single level of JSON files', () => {
     fancyIt()('should return all the JSON files', async () => {
-      vol.fromJSON(
-        {
-          'foo.json': '',
-          'bar.json': '',
-          'baz.json': '',
-        },
-        '/mock' // current working directory
-      );
+      mockedReaddirSync.mockReturnValueOnce([
+        getFile('foo.json'),
+        getFile('bar.json'),
+        getFile('baz.json'),
+      ]);
       expect(fileDepthSearch('/mock').sort()).toEqual([
         '/mock/bar.json',
         '/mock/baz.json',
@@ -32,15 +25,12 @@ describe('File', () => {
 
   describe('when the directory contains nested directories', () => {
     fancyIt()('should return all the JSON files', async () => {
-      vol.fromJSON(
-        {
-          'foo.json': '',
-          'baz.json': '',
-          'dir1/bar.json': '',
-          'dir1/dir2/pow.json': '',
-        },
-        '/mock'
-      );
+      mockedReaddirSync.mockReturnValueOnce([
+        getFile('foo.json'),
+        getFile('baz.json'),
+        getFile('dir1/bar.json'),
+        getFile('dir1/dir2/pow.json'),
+      ]);
       expect(fileDepthSearch('/mock').sort()).toEqual([
         '/mock/baz.json',
         '/mock/dir1/bar.json',
@@ -50,33 +40,19 @@ describe('File', () => {
     });
   });
 
-  describe('when the directory contains no files', () => {
-    fancyIt()('should return all the JSON files', async () => {
-      vol.fromJSON(
-        {
-          dir1: null,
-        },
-        '/mock'
-      );
-      expect(fileDepthSearch('/mock')).toEqual([]);
-    });
-  });
-
   describe('when project mix files', () => {
     fancyIt()('should return all the JSON files', async () => {
-      vol.fromJSON(
-        {
-          'text.txt': '',
-          'foo.json': '',
-          'no-extension': '',
-          'baz.json': '',
-          'dir1/bar.txt': '',
-          'dir1/bar.json': '',
-          'dir1/dir2': null,
-          'dir1/dir2/pow.json': '',
-        },
-        '/mock'
-      );
+      mockedReaddirSync.mockReturnValueOnce([
+        getFile('text.nojson'),
+        getFile('foo.json'),
+        getFile('no-extension'),
+        getFile('baz.json'),
+        getFile('emptydir'),
+        getFile('dir1/bar.nojson'),
+        getFile('dir1/bar.json'),
+        getFile('dir1/dir2/pow.json'),
+      ]);
+
       expect(fileDepthSearch('/mock').sort()).toEqual([
         '/mock/baz.json',
         '/mock/dir1/bar.json',
