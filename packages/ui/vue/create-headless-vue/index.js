8fc9c5910925a16a0c4e469784e0ca4708778908
Louis Bompart <lbompart@coveo.com>
2022-10-26 14:35:32 -0400

    tentative

--- packages/ui/vue/create-headless-vue/index.js
+++ packages/ui/vue/create-headless-vue/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import {dirname, resolve} from 'node:path';
-import {cpSync} from 'node:fs';
+import {dirname, resolve, join} from 'node:path';
+import {cpSync, renameSync} from 'node:fs';
 import {cwd} from 'node:process';
 import {fileURLToPath} from 'node:url';
 
@@ -8,3 +8,5 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const templateRelativeDir = 'template';
 const templateDirPath = resolve(__dirname, templateRelativeDir);
 cpSync(templateDirPath, cwd(), {recursive: true});
+// https://github.com/npm/cli/issues/5756
+renameSync(join(cwd(), '.npmignore'), join(cwd(), '.gitignore'));
