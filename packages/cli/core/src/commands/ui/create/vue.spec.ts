e7cd039f697d0a71610ee094e3fb60225f0e0213
Louis Bompart <lbompart@coveo.com>
2022-10-26 12:13:46 -0400

    test(vue): unit-tests

--- packages/cli/core/src/commands/ui/create/vue.spec.ts
+++ packages/cli/core/src/commands/ui/create/vue.spec.ts
@@ -10,6 +10,7 @@ jest.mock('@coveo/cli-commons/preconditions/authenticated');
 jest.mock('@coveo/cli-commons/platform/authenticatedClient');
 jest.mock('../../../lib/utils/misc');
 jest.mock('@coveo/platform-client');
+jest.mock('node:fs');
 
 import {test} from '@oclif/test';
 import {spawnProcess} from '../../../lib/utils/process';
@@ -27,6 +28,14 @@ import {
 import {getPackageVersion} from '../../../lib/utils/misc';
 import {configurationMock} from '../../../__stub__/configuration';
 import {mockPreconditions} from '@coveo/cli-commons/preconditions/mockPreconditions';
+import {
+  Stats,
+  statSync,
+  readdirSync,
+  mkdirSync,
+  Dirent,
+  writeFileSync,
+} from 'node:fs';
 
 describe('ui:create:vue', () => {
   const mockedConfig = jest.mocked(Config);
@@ -36,10 +45,13 @@ describe('ui:create:vue', () => {
   const mockedAuthenticatedClient = jest.mocked(AuthenticatedClient);
   const mockedIsNpxInstalled = jest.mocked(IsNpxInstalled);
   const mockedIsNodeVersionInRange = jest.mocked(IsNodeVersionInRange);
-  const vueCliPackage = '@vue/cli';
   const mockedApiKeyPrivilege = jest.mocked(HasNecessaryCoveoPrivileges);
   const mockedIsAuthenticated = jest.mocked(IsAuthenticated);
-
+  const mockedStatSync = jest.mocked(statSync);
+  const mockedReadDirSync = jest.mocked(readdirSync);
+  const mockedMkdirSync = jest.mocked(mkdirSync);
+  const mockedWriteFileSync = jest.mocked(writeFileSync);
+  const fooBarDirectoryMatcher = /(\w:)?(\\\\|\/)foo(\\\\|\/)bar(\\\\|\/)myapp/;
   const mockedCreateImpersonateApiKey = jest.fn();
   const preconditionStatus = {
     node: true,
@@ -98,6 +110,12 @@ describe('ui:create:vue', () => {
     );
   };
 
+  const doMockNodeProcess = () => {
+    jest.mock('node:process', () => ({
+      cwd: () => '/foo/bar',
+    }));
+  };
+
   beforeEach(() => {
     doMockedGetPackageVersion();
     doMockSpawnProcess();
@@ -105,6 +123,7 @@ describe('ui:create:vue', () => {
     doMockConfiguration();
     doMockAuthenticatedClient();
     doMockPreconditions();
+    doMockNodeProcess();
     preconditionStatus.node = true;
     preconditionStatus.npx = true;
     preconditionStatus.apiKey = true;
@@ -146,43 +165,120 @@ describe('ui:create:vue', () => {
     .catch(/Missing 1 required arg/)
     .it('requires application name argument');
 
-  test
-    .stdout()
-    .stderr()
-    .command(['ui:create:vue', 'myapp'])
-    .it('should start 2 spawn processes with default preset arguments', () => {
-      expect(mockedSpawnProcess).toHaveBeenCalledTimes(2);
-      expect(mockedSpawnProcess).nthCalledWith(
-        1,
-        expect.stringContaining('npx'),
-        [
-          `${vueCliPackage}@1.0.0`,
-          'create',
-          'myapp',
-          '--inlinePreset',
-          expect.objectContaining({}),
-          '--skipGetStarted',
-          '--bare',
-        ],
-        expect.objectContaining({})
-      );
-      expect(mockedSpawnProcess).nthCalledWith(
-        2,
-        expect.stringContaining('npx'),
-        [
-          `${vueCliPackage}@1.0.0`,
-          'add',
-          '@coveo/vue-cli-plugin-typescript@1.0.0',
-          '--orgId',
-          'my-org',
-          '--apiKey',
-          'foo',
-          '--platformUrl',
-          'https://platformdev.cloud.coveo.com',
-          '--user',
-          'bob@coveo.com',
-        ],
-        expect.objectContaining({cwd: 'myapp'})
-      );
+  describe('when a file with the projectName exists', () => {
+    beforeEach(() => {
+      const mockedIsDir = jest.fn();
+      mockedIsDir.mockReturnValue(false);
+      mockedStatSync.mockReturnValue({
+        isDirectory: mockedIsDir,
+      } as unknown as Stats);
     });
+    test
+      .stdout()
+      .stderr()
+      .command(['ui:create:vue', 'myapp'])
+      .catch((err) => {
+        expect(err.message).toMatchSnapshot();
+      })
+      .it('should exit with an error', () => {
+        expect(mockedSpawnProcess).not.toBeCalled();
+        expect(mockedStatSync).toBeCalledWith(
+          expect.stringMatching(fooBarDirectoryMatcher),
+          {
+            throwIfNoEntry: false,
+          }
+        );
+      });
+  });
+
+  describe('when a directory with the projectName exists', () => {
+    beforeEach(() => {
+      const mockedIsDir = jest.fn();
+      mockedIsDir.mockReturnValue(true);
+      mockedStatSync.mockReturnValue({
+        isDirectory: mockedIsDir,
+      } as unknown as Stats);
+    });
+    describe('when the directory is not empty', () => {
+      beforeEach(() => {
+        mockedReadDirSync.mockReturnValue([{} as unknown as Dirent]);
+      });
+      test
+        .stdout()
+        .stderr()
+        .command(['ui:create:vue', 'myapp'])
+        .catch((err) => {
+          expect(err.message).toMatchSnapshot();
+        })
+        .it('should exit with an error', () => {
+          expect(mockedSpawnProcess).not.toBeCalled();
+          expect(mockedStatSync).toBeCalledWith(
+            expect.stringMatching(fooBarDirectoryMatcher),
+            {
+              throwIfNoEntry: false,
+            }
+          );
+          expect(mockedReadDirSync).toBeCalledWith(
+            expect.stringMatching(fooBarDirectoryMatcher),
+            {
+              withFileTypes: true,
+            }
+          );
+        });
+    });
+    describe('when the directory is empty', () => {
+      beforeEach(() => {
+        mockedReadDirSync.mockReturnValue([]);
+      });
+      test
+        .stdout()
+        .stderr()
+        .command(['ui:create:vue', 'myapp'])
+        .it('should use the dir and init the project', () => {
+          expect(mockedStatSync).toBeCalledWith(
+            expect.stringMatching(fooBarDirectoryMatcher),
+            {
+              throwIfNoEntry: false,
+            }
+          );
+          expect(mockedReadDirSync).toBeCalledWith(
+            expect.stringMatching(fooBarDirectoryMatcher),
+            {
+              withFileTypes: true,
+            }
+          );
+          expect(mockedSpawnProcess).toBeCalledTimes(2);
+          expect(mockedSpawnProcess.mock.calls[0]).toMatchSnapshot();
+          expect(mockedSpawnProcess.mock.calls[1]).toMatchSnapshot();
+          expect(mockedWriteFileSync).toBeCalledTimes(1);
+          expect(mockedWriteFileSync.mock.calls[0]).toMatchSnapshot();
+        });
+    });
+  });
+
+  describe('when the projectName does not exists yet', () => {
+    beforeEach(() => {
+      mockedStatSync.mockReturnValue(undefined);
+    });
+    test
+      .stdout()
+      .stderr()
+      .command(['ui:create:vue', 'myapp'])
+      .it('should create the dir and init the project', () => {
+        expect(mockedStatSync).toBeCalledWith(
+          expect.stringMatching(fooBarDirectoryMatcher),
+          {
+            throwIfNoEntry: false,
+          }
+        );
+        expect(mockedMkdirSync).toBeCalledWith(
+          expect.stringMatching(fooBarDirectoryMatcher)
+        );
+        expect(mockedSpawnProcess).toBeCalledTimes(2);
+        expect(mockedSpawnProcess.mock.calls[0]).toMatchSnapshot();
+        expect(mockedSpawnProcess.mock.calls[1]).toMatchSnapshot();
+        expect(mockedWriteFileSync).toBeCalledTimes(1);
+        expect(mockedWriteFileSync.mock.calls[0]).toMatchSnapshot();
+      });
+  });
 });
