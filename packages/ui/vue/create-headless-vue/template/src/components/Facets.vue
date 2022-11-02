e19d35eb60ebaaa3ff99956b1ef5e4233a1be63c
Louis Bompart <lbompart@coveo.com>
2022-11-02 16:53:43 -0400

    feat(vue)!: migrate to vue3
    BREAKING CHANGE: Deprecate vue2 plugin
    BREAKING CHANGE: v1 release of vue3 npm initializer package 
    
    https://coveord.atlassian.net/browse/CDX-813

---
+++ packages/ui/vue/create-headless-vue/template/src/components/Facets.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <v-row>
+      <v-col cols="12">
+        <h4 class="text-h5">Refine By</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" class="p1">
+        <v-expansion-panels>
+          <CoveoFacet field="objecttype" title="Object Type" />
+          <CoveoFacet field="filetype" title="File Type" />
+          <CoveoFacet field="author" title="Author" />
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+
+<script lang="ts">
+import CoveoFacet from "./Facet.vue";
+
+export default {
+  name: "CoveoFacets",
+  components: { CoveoFacet },
+  data: function () {
+    return {
+      open: false,
+    };
+  },
+  methods: {
+    onClick: function () {
+      this.open = true;
+    },
+  },
+};
+</script>
---
+++ packages/ui/vue/create-headless-vue/template/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// @ is an alias to /src
+import SearchPage from "@/components/SearchPage.vue";
+</script>
+<template>
+  <v-layout>
+    <v-main>
+      <v-container fluid>
+        <Suspense>
+          <SearchPage />
+        </Suspense>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
