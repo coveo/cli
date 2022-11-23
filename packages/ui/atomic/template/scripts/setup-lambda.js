60d2d7e604d776e88a455983395fabdbb37fc007
Louis Bompart <lbompart@coveo.com>
2022-11-23 15:00:10 -0500

    attempt3

--- packages/ui/atomic/template/scripts/setup-lambda.js
+++ packages/ui/atomic/template/scripts/setup-lambda.js
@@ -1,4 +1,5 @@
 const {linkSync} = require('fs');
+const {resolve} = require('path');
 function linSearchTokenLambdaToRoot() {
   linkSync(
     resolve('node_modules', '@coveo', 'search-token-lambda'),
