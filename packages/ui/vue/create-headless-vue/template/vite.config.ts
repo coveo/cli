7ff133805d059a2f8154ca987ff3180e8efebb3f
Louis Bompart <lbompart@coveo.com>
2023-01-19 16:31:43 -0500

    refactor rootdir in viteconfig

--- packages/ui/vue/create-headless-vue/template/vite.config.ts
+++ packages/ui/vue/create-headless-vue/template/vite.config.ts
@@ -1,16 +1,19 @@
 import { fileURLToPath, URL } from "node:url";
+import { dirname, resolve } from "node:path";
 import { defineConfig, loadEnv } from "vite";
 import coveoPlugin from "./src/server";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+const rootDir = dirname(fileURLToPath(import.meta.url));
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [coveoPlugin(loadEnv("", process.cwd())), vue(), vueJsx()],
+  plugins: [coveoPlugin(loadEnv("", rootDir)), vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src/ui", import.meta.url)),
-      commons: fileURLToPath(new URL("./src/commons", import.meta.url)),
+      "@": resolve(rootDir, "./src/ui"),
+      commons: resolve(rootDir, "./src/commons"),
     },
   },
 });
