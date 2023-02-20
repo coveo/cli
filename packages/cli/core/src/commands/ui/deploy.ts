f5d069334c8c5eb58251a83d15a6e2f0a9adc611
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2023-02-20 15:32:37 -0500

    update precondition import

--- packages/cli/core/src/commands/ui/deploy.ts
+++ packages/cli/core/src/commands/ui/deploy.ts
@@ -7,7 +7,7 @@ import {
   HasNecessaryCoveoPrivileges,
   IsAuthenticated,
   Preconditions,
-} from '@coveo/cli-commons/preconditions';
+} from '@coveo/cli-commons/preconditions/index';
 import {HostedPage, New} from '@coveo/platform-client';
 import {createSearchPagesPrivilege} from '@coveo/cli-commons/preconditions/platformPrivilege';
 import {Flags} from '@oclif/core';
