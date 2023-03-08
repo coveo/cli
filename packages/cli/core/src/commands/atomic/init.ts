ab95dfa132ad4dda764f468e1788d1453b40399c
Louis Bompart <lbompart@coveo.com>
2023-03-08 17:55:43 -0500

    feat(atomic): atomic:init 1/2: app

---
+++ packages/cli/core/src/commands/atomic/init.ts
@@ -0,0 +1,49 @@
+import {Config} from '@coveo/cli-commons/config/config';
+import {Command, Flags} from '@oclif/core';
+import {createAtomicApp} from '../../lib/atomic/createAtomicProject';
+
+export default class AtomicInit extends Command {
+  static description = 'Scaffold a project to work with Coveo Atomic Framework';
+
+  static examples = [
+    '<%= config.bin %> <%= command.id %> --type=app myAwesomeSearchPage',
+    '<%= config.bin %> <%= command.id %> --type=lib myCustomAtomicComponentsLibrary',
+  ];
+
+  static flags = {
+    type: Flags.string({
+      description: 'the kind of project to initialize',
+      options: ['app', 'application', 'lib', 'library'],
+    }),
+  };
+
+  static args = [{name: 'name', description: 'the name of your project'}];
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(AtomicInit);
+
+    switch (flags.type) {
+      case 'app':
+      case 'application':
+        await this.createAtomicApp(args.name);
+        break;
+      case 'lib':
+      case 'library':
+      //TODO
+      default:
+        break;
+    }
+  }
+  private async createAtomicApp(projectName: string) {
+    const cfg = this.configuration.get();
+    await createAtomicApp({
+      initializerVersion: 'latest',
+      projectName,
+      cfg,
+    });
+  }
+
+  private get configuration() {
+    return new Config(this.config.configDir);
+  }
+}
