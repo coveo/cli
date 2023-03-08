631e54f62b6a010a32bd77fde6032f61a8f86a2d
Louis Bompart <lbompart@coveo.com>
2023-03-08 18:45:14 -0500

    feat(atomic): atomic:cmp

---
+++ packages/cli/core/src/commands/atomic/cmp.ts
@@ -0,0 +1,65 @@
+import {CLICommand} from '@coveo/cli-commons/command/cliCommand';
+import {UnknownError} from '@coveo/cli-commons/errors/unknownError';
+import {Flags} from '@oclif/core';
+import inquirer from 'inquirer';
+import {appendCmdIfWindows} from '../../lib/utils/os';
+import {spawnProcess} from '../../lib/utils/process';
+
+export default class AtomicInit extends CLICommand {
+  public static description =
+    'Scaffold a new custom component in your Coveo Atomic Component Library';
+
+  public static examples = [
+    '<%= config.bin %> <%= command.id %> --type=page myAwesomeComponent',
+    '<%= config.bin %> <%= command.id %> --type=result mySuperResultComponent',
+  ];
+
+  public static flags = {
+    type: Flags.string({
+      description: 'the kind of component to initialize',
+      options: ['page', 'result'],
+    }),
+  };
+
+  public static args = [
+    {name: 'name', description: 'the name of your component', required: true},
+  ];
+
+  public async run(): Promise<void> {
+    const {initializer, name} = await this.getSpawnOptions();
+
+    const cliArgs = ['init', initializer, name];
+    await spawnProcess(appendCmdIfWindows`npm`, cliArgs);
+  }
+
+  private async getSpawnOptions() {
+    const {args, flags} = await this.parse(AtomicInit);
+    const type = flags.type || (await this.askType());
+
+    const initializer = this.getInitializerPackage(type);
+    return {initializer, name: args.name};
+  }
+
+  private async askType(): Promise<string> {
+    const responses = await inquirer.prompt([
+      {
+        name: 'type',
+        message: 'What kind of component do you want to scaffold?',
+        type: 'list',
+        choices: [{name: 'result'}, {name: 'page'}],
+      },
+    ]);
+    return responses.type;
+  }
+
+  private getInitializerPackage(type: string): string {
+    switch (type) {
+      case 'page':
+        return '@coveo/atomic-component';
+      case 'result':
+        return '@coveo/atomic-component';
+      default:
+        throw new UnknownError();
+    }
+  }
+}
