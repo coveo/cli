6d8c2ba5b4b7e3c704c26c1dd38b06aafc590478
Louis Bompart <lbompart@coveo.com>
2023-03-10 15:12:17 -0500

    chore: remove everything related to netlify (#1142)
    <!-- For Coveo Employees only. Fill this section.
    
    KIT-2349
    -->
    
    ## Proposed changes
    
    Remove everything related to Netlify.

--- packages/ui/search-token-lambda/functions/token/areEnvironmentVariablesSet.ts
+++
@@ -1,5 +0,0 @@
-export function areEnvironmentVariablesSet() {
-  return Boolean(
-    process.env.ORGANIZATION_ID && process.env.API_KEY && process.env.USER_EMAIL
-  );
-}
--- packages/ui/search-token-lambda/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-  clearMocks: true,
-  silent: true,
-  testTimeout: 60e3,
-};
