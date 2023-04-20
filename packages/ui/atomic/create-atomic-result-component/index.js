64bd9b8420fcaddf5bb0cebeffe9287b1ce23367
Louis Bompart <lbompart@coveo.com>
2023-04-20 16:21:21 -0400

    resulttoo
    https://coveord.atlassian.net/browse/CDX-1400

--- packages/ui/atomic/create-atomic-result-component/index.js
+++ packages/ui/atomic/create-atomic-result-component/index.js
@@ -54,7 +54,7 @@ const transform = (transformers) => {
   }
 };
 
-// Taken from Stencil: https://github.com/ionic-team/stencil/blob/main/src/utils/validation.ts
+// Adapted from Stencil: https://github.com/ionic-team/stencil/blob/main/src/utils/validation.ts
 /**
  * Validates that a component tag meets required naming conventions to be used for a web component
  * @param tag the tag to validate
@@ -62,44 +62,32 @@ const transform = (transformers) => {
  */
 const ensureComponentValidity = (tag) => {
   const errors = [];
-  if (tag !== tag.toLowerCase()) {
-    errors.push(new Error(`Tag cannot contain upper case characters`));
+  const alphaAndHyphenOnly = /^[a-z\-]+$/;
+  const forbiddenLeadingHyphen = /^-/;
+  const forbiddenTrailingHyphen = /-$/;
+  const forbiddenMultiHyphen = /-{2,}/;
+  const shouldContainAtLeastOneHyphen = /-/;
+
+  if (!alphaAndHyphenOnly.test(tag)) {
+    errors.push(`"${tag}" can only contain lower case alphabetical characters`);
   }
-  if (tag.length === 0) {
-    errors.push(new Error(`Received empty tag value`));
+  if (forbiddenLeadingHyphen.test(tag)) {
+    errors.push(`"${tag}" cannot start with a dash (-)`);
   }
-  if (tag.includes(' ')) {
-    errors.push(new Error(`"${tag}" tag cannot contain a space`));
+  if (forbiddenTrailingHyphen.test(tag)) {
+    errors.push(`"${tag}" cannot end with a dash (-)`);
   }
-  if (tag.includes(',')) {
-    errors.push(new Error(`"${tag}" tag cannot be used for multiple tags`));
-  }
-  const invalidChars = tag.replace(/\w|-|\s/g, '');
-  if (invalidChars !== '') {
+  if (!shouldContainAtLeastOneHyphen.test(tag)) {
     errors.push(
-      new Error(`"${tag}" tag contains invalid characters: ${invalidChars}`)
+      `"${tag}" must contain a dash (-) to work as a valid web component`
     );
   }
-  if (!tag.includes('-')) {
+  if (forbiddenMultiHyphen.test(tag)) {
     errors.push(
-      new Error(
-        `"${tag}" tag must contain a dash (-) to work as a valid web component`
-      )
+      `"${tag}" cannot contain multiple dashes (--) next to each other`
     );
   }
-  if (tag.includes('--')) {
-    errors.push(
-      new Error(
-        `"${tag}" tag cannot contain multiple dashes (--) next to each other`
-      )
-    );
-  }
-  if (tag.startsWith('-')) {
-    errors.push(new Error(`"${tag}" tag cannot start with a dash (-)`));
-  }
-  if (tag.endsWith('-')) {
-    errors.push(new Error(`"${tag}" tag cannot end with a dash (-)`));
-  }
+
   if (errors.length > 0) {
     throw new AggregateError(errors, 'Invalid component tag name');
   }
