46cfeb24c70ea96467e1d705537790dfaa929957
Louis Bompart <lbompart@coveo.com>
2023-04-20 16:54:08 -0400

    fix(cli-core,cli-source,ui-react): enforce LTS (#1275)
    <!-- For Coveo Employees only. Fill this section.
    
    CDX-1413
    
    -->
    
    ## Proposed changes
    
    Warn the user if the version of Node used by the CLI does not match our
    engines requirement
    
    This come from one of our internal user using some version of Node 18
    pre-LTS.
    This version of Node he was using used a buggy implementation of fetch,
    which was causing the Authentication check to fail.
    
    ## Testing
    
    - [x] Unit Tests:
    - [x] Manual Tests:
      Install unsupported Node version (let's say 20) and run the CLI

---
+++ packages/cli/core/src/hooks/prerun/checkNodeVersion.ts
@@ -0,0 +1,14 @@
+import {Hook} from '@oclif/core';
+import {satisfies} from 'semver';
+import dedent from 'ts-dedent';
+const hook: Hook<'prerun'> = function () {
+  if (!satisfies(process.version, this.config.pjson.engines.node)) {
+    this.warn(
+      dedent`NodeJS ${process.version} is not supported. The Coveo CLI might malfunction.
+      Please update your NodeJS installation to the latest LTS version.`
+    );
+  }
+  return Promise.resolve();
+};
+
+export default hook;
