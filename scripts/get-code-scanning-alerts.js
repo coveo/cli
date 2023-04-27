cf0cab7c58dad6eb56e52764d70a85790e2d104d
Louis Bompart <lbompart@coveo.com>
2023-04-27 13:56:31 -0400

    ci: replace snyk remnant by codeql

--- scripts/get-code-scanning-alerts.js
+++
@@ -1,8 +0,0 @@
-const {getSnykCodeAlerts} = require('./github-client');
-
-const isHighSeverity = (alert) => alert.rule.severity === 'error';
-(async () => {
-  const alerts = (await getSnykCodeAlerts()).data;
-  const hasAtLeastOneHighAlert = alerts.some(isHighSeverity);
-  process.exit(hasAtLeastOneHighAlert ? 1 : 0);
-})();
