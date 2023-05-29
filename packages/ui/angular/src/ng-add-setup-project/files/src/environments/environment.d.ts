63db6fc49c59e305f533b33e8dcb4a0b7c359516
Olivier Lamothe <olamothe@coveo.com>
2023-05-29 15:38:16 -0400

    remove headless ref

--- packages/ui/angular/src/ng-add-setup-project/files/src/environments/environment.d.ts
+++ packages/ui/angular/src/ng-add-setup-project/files/src/environments/environment.d.ts
@@ -1,10 +1,8 @@
-import {PlatformEnvironment} from '@coveo/headless';
-
 export declare const environment: {
   production: boolean;
   customTokenEndpoint: string;
   defaultTokenEndpoint: string;
   platformUrl: string;
   organizationId: string;
-  platformEnvironment: PlatformEnvironment;
+  platformEnvironment: 'prod' | 'hipaa' | 'stg' | 'dev';
 };
