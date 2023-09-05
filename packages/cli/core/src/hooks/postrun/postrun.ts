bf50b148591445bd22a24c435931eb03f40cf36c
Louis Bompart <lbompart@coveo.com>
2023-09-05 15:42:24 -0400

    chore(cli-core): upgrade amplitude+refactor
    https://coveord.atlassian.net/browse/CDX-1472

--- packages/cli/core/src/hooks/postrun/postrun.ts
+++ packages/cli/core/src/hooks/postrun/postrun.ts
@@ -2,7 +2,7 @@ import {Hook} from '@oclif/core';
 import {flush} from '@coveo/cli-commons/analytics/amplitudeClient';
 
 const hook: Hook<'postrun'> = async function (_options) {
-  await flush();
+  flush();
 };
 
 export default hook;
