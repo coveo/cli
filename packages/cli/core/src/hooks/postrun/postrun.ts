9c501c6e896b3e0bbe0a78672a5ed2ee54040ae0
Louis Bompart <lbompart@coveo.com>
2023-09-08 16:13:39 -0400

    shush-promise
    https://coveord.atlassian.net/browse/CDX-1472

--- packages/cli/core/src/hooks/postrun/postrun.ts
+++ packages/cli/core/src/hooks/postrun/postrun.ts
@@ -1,8 +1,9 @@
 import {Hook} from '@oclif/core';
 import {flush} from '@coveo/cli-commons/analytics/amplitudeClient';
 
-const hook: Hook<'postrun'> = async function (_options) {
+const hook: Hook<'postrun'> = function (_options) {
   flush();
+  return Promise.resolve();
 };
 
 export default hook;
