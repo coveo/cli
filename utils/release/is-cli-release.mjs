9c0257e79fa8b5fef58bd5f65509a400cd8a312f
Louis Bompart <lbompart@coveo.com>
2023-10-26 14:25:44 -0400

    fix: invokeiscli

--- utils/release/is-cli-release.mjs
+++ utils/release/is-cli-release.mjs
@@ -2,12 +2,10 @@ import {readFileSync} from 'node:fs';
 import {setOutput} from '@actions/core';
 
 const CLI_PKG_MATCHER = /^@coveo\/cli@(?<version>\d+\.\d+\.\d+)$/gm;
-async () => {
-  const packagesReleased = readFileSync('.git-message', {
-    encoding: 'utf-8',
-  }).trim();
+const packagesReleased = readFileSync('.git-message', {
+  encoding: 'utf-8',
+}).trim();
 
-  // If `@coveo/cli` has not been released we should not build it etc.
-  const shouldReleaseCli = CLI_PKG_MATCHER.test(packagesReleased);
-  setOutput('shouldReleaseCli', shouldReleaseCli);
-};
+// If `@coveo/cli` has not been released we should not build it etc.
+const shouldReleaseCli = CLI_PKG_MATCHER.test(packagesReleased);
+setOutput('shouldReleaseCli', shouldReleaseCli);
