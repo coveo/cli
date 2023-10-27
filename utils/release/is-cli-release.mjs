caddabaa318999c849d1c8e8ce62cfbec90feab5
Louis Bompart <lbompart@coveo.com>
2023-10-27 03:26:56 -0400

    chore: stabilize

--- utils/release/is-cli-release.mjs
+++ utils/release/is-cli-release.mjs
@@ -1,13 +1,13 @@
+#!/usr/bin/env node
+
 import {readFileSync} from 'node:fs';
 import {setOutput} from '@actions/core';
 
 const CLI_PKG_MATCHER = /^@coveo\/cli@(?<version>\d+\.\d+\.\d+)$/gm;
-async () => {
-  const packagesReleased = readFileSync('.git-message', {
-    encoding: 'utf-8',
-  }).trim();
+const packagesReleased = readFileSync('.git-message', {
+  encoding: 'utf-8',
+}).trim();
 
-  // If `@coveo/cli` has not been released we should not build it etc.
-  const shouldReleaseCli = CLI_PKG_MATCHER.test(packagesReleased);
-  setOutput('shouldReleaseCli', shouldReleaseCli);
-};
+// If `@coveo/cli` has not been released we should not build it etc.
+const shouldReleaseCli = CLI_PKG_MATCHER.test(packagesReleased);
+setOutput('shouldReleaseCli', shouldReleaseCli);
