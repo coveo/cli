93c444dbc7d63027ad884b75e52f9a718770557e
Louis Bompart <lbompart@coveo.com>
2024-03-14 13:51:23 -0400

    snap update

--- packages/cli/core/src/lib/ui/shared.ts
+++ packages/cli/core/src/lib/ui/shared.ts
@@ -1,37 +1,38 @@
-import {confirm, promptChoices, prompt} from '@coveo/cli-commons/utils/ux';
+// import {confirm, promptChoices, prompt} from '@coveo/cli-commons/utils/ux';
 import PlatformClient from '@coveo/platform-client';
-import inquirer from 'inquirer';
+// import inquirer from 'inquirer';
 
-const manuallyEnterSearchHub = () =>
-  prompt('Enter the search hub you want to use', '');
+// const manuallyEnterSearchHub = () =>
+//   prompt('Enter the search hub you want to use', '');
 
 export async function promptForSearchHub(client: PlatformClient) {
-  const createSearchHub = await confirm(
-    'An API key will be created against your organization. We strongly recommends that you associate this API key with a search hub. Would you like to do so now ? (y/n)',
-    false
-  );
-  if (!createSearchHub) {
-    return;
-  }
+  return '';
+  // const createSearchHub = await confirm(
+  //   'An API key will be created against your organization. We strongly recommends that you associate this API key with a search hub. Would you like to do so now ? (y/n)',
+  //   false
+  // );
+  // if (!createSearchHub) {
+  //   return;
+  // }
 
-  const availableHubs = await client.searchUsageMetrics.searchHubs.list();
-  if (availableHubs.hubs.length === 0) {
-    return await manuallyEnterSearchHub();
-  }
+  // const availableHubs = await client.searchUsageMetrics.searchHubs.list();
+  // if (availableHubs.hubs.length === 0) {
+  //   return await manuallyEnterSearchHub();
+  // }
 
-  const choice = await promptChoices(
-    'Use an existing search hub, or create a new one?',
-    [
-      new inquirer.Separator(),
-      'Create a new one',
-      new inquirer.Separator(),
-      ...availableHubs.hubs.map((hub) => hub.name),
-    ],
-    ''
-  );
+  // const choice = await promptChoices(
+  //   'Use an existing search hub, or create a new one?',
+  //   [
+  //     new inquirer.Separator(),
+  //     'Create a new one',
+  //     new inquirer.Separator(),
+  //     ...availableHubs.hubs.map((hub) => hub.name),
+  //   ],
+  //   ''
+  // );
 
-  if (choice === 'Create a new one') {
-    return await manuallyEnterSearchHub();
-  }
-  return choice;
+  // if (choice === 'Create a new one') {
+  //   return await manuallyEnterSearchHub();
+  // }
+  // return choice;
 }
