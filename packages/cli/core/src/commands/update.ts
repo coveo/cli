95f1b5458ed276cd033718155a5ae95b1bb204f1
Louis Bompart <lbompart@coveo.com>
2025-03-17 10:42:08 -0400

    feat: noop update cmd (#1502)
    https://coveord.atlassian.net/browse/CDX-1614
    
    ---------
    
    Co-authored-by: Alex Prudhomme <78121423+alexprudhomme@users.noreply.github.com>

---
+++ packages/cli/core/src/commands/update.ts
@@ -0,0 +1,12 @@
+import {CLICommand} from '@coveo/cli-commons/command/cliCommand';
+
+export default class Create extends CLICommand {
+  public static description = 'Deprecated';
+
+  public async run() {
+    this.log(
+      `The update command is deprecated. If you are using a binary install, please uninstall the Coveo CLI and reinstall it using npm. If you are already using an npm install, please update the Coveo CLI using npm.`
+`
+    );
+  }
+}
