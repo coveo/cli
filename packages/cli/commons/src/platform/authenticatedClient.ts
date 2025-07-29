cdaf72f9732641a3d4183b987b54c37c0ae2754f
fbeaudoincoveo <fbeaudoin@coveo.com>
2025-07-29 10:02:09 -0400

    Fix impersonate API key creation issue

--- packages/cli/commons/src/platform/authenticatedClient.ts
+++ packages/cli/commons/src/platform/authenticatedClient.ts
@@ -72,9 +72,7 @@ export class AuthenticatedClient {
           },
         },
       }),
-      privileges: [
-        {targetDomain: 'IMPERSONATE', targetId: '*', owner: 'SEARCH_API'},
-      ],
+      apiKeyTemplateId: 'AuthenticatedSearch'
     });
   }
 
